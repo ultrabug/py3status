1d0df93fda422538118984e801553d074d976117
Ultrabug <ultrabug@gentoo.org>
2013-04-07 17:11:43 +0200

    PEP8 identation and python3 compatible examples

--- examples/empty_class.py
+++ examples/empty_class.py
@@ -1,13 +1,13 @@
 class Py3status:
-	"""
-	Empty and basic py3status class
-	"""
-	def empty(self, json, i3status_config):
-		"""
-		This method will return an empty text message, so it will NOT be displayed.
-		If you want something displayed you should write something in the 'full_text' key of your response.
-		See the i3bar protocol spec for more information:
-			http://i3wm.org/docs/i3bar-protocol.html
-		"""
-		response = {'full_text' : '', 'name' : 'empty'}
-		return (0, response)
+    """
+    Empty and basic py3status class
+    """
+    def empty(self, json, i3status_config):
+        """
+        This method will return an empty text message, so it will NOT be displayed.
+        If you want something displayed you should write something in the 'full_text' key of your response.
+        See the i3bar protocol spec for more information:
+            http://i3wm.org/docs/i3bar-protocol.html
+        """
+        response = {'full_text' : '', 'name' : 'empty'}
+        return (0, response)
