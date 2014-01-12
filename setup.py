879ff53e4f338ea6754337d91ef100154cf3127a
Ultrabug <ultrabug@gentoo.org>
2014-01-12 21:32:48 +0100

    version 1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='py3status',
-    version='1.1',
+    version='1.2',
     author='Ultrabug',
     author_email='ultrabug@ultrabug.net',
     description='py3status is an extensible i3status wrapper written in python',
