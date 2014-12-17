ab26b713cf1b3e560770adb73542a19152f4057d
Ultrabug <ultrabug@gentoo.org>
2014-12-17 19:27:19 +0100

    module scratchpad-counter more QA

--- py3status/modules/scratchpad-counter.py
+++ py3status/modules/scratchpad-counter.py
@@ -31,7 +31,7 @@ class Py3status:
     def __init__(self):
         self.count = -1
 
-    def scratchpad_counter(self, i3status_output_json, i3status_config):
+    def scratchpad_counter(self, i3s_output_list, i3s_config):
         count = len(find_scratch(i3.get_tree()).get("floating_nodes", []))
 
         if self.count != count:
