738137fe2b958945a65ba9783b2bd0603e0c62e5
Ultrabug <ultrabug@gentoo.org>
2015-03-26 14:32:51 +0100

    drop python2.5 from classifiers

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
