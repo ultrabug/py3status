f46c6a6569a96576fbaca0e6f8ec93f90b9883a7
Ultrabug <ultrabug@gentoo.org>
2015-07-09 11:09:19 +0200

    external_script and static_string modules code QA

--- py3status/modules/external_script.py
+++ py3status/modules/external_script.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Display output of given script
+Display output of given script.
 
-Display output of any executable script set by script_path
-Pay attention. The output must be one liner, or will break your i3status
-The script should not have any parameters, but it could work
+Display output of any executable script set by 'script_path'.
+Pay attention. The output must be one liner, or will break your i3status !
+The script should not have any parameters, but it could work.
 
 Configuration parameters:
     - cache_timeout : how often we refresh this module in seconds
-    - script_path   : script you want to show output of (compulsory)
     - color         : color of printed text
-    - on_click      : read goo.gl/u10n0x
     - format        : see placeholders below
+    - script_path   : script you want to show output of (compulsory)
 
 Format of status string placeholders:
     {output} - output of script given by "script_path"
@@ -19,9 +18,9 @@ Format of status string placeholders:
 i3status.conf example:
 
 external_script {
-    script_path = "/usr/bin/whoami"
-    format = "my name is {output}"
     color = "#00FF00"
+    format = "my name is {output}"
+    script_path = "/usr/bin/whoami"
 
 @author frimdo ztracenastopa@centrum.cz
 """
@@ -31,30 +30,31 @@ from time import time
 
 
 class Py3status:
+    """
+    """
+    # available configuration parameters
     cache_timeout = 15
-    format = '{output}'
     color = None
+    format = '{output}'
     script_path = None
 
     def external_script(self, i3s_output_list, i3s_config):
-
         if self.script_path:
-
-            return_value = subprocess.check_output(self.script_path, shell=True, universal_newlines=True)
+            return_value = subprocess.check_output(self.script_path,
+                                                   shell=True,
+                                                   universal_newlines=True)
             response = {
                 'cached_until': time() + self.cache_timeout,
-                'full_text': self.format.format(
-                    output=return_value.rstrip()),
-                'color': self.color
-                }
-
+                'color': self.color,
+                'full_text': self.format.format(output=return_value.rstrip())
+            }
         else:
             response = {
                 'cached_until': time() + self.cache_timeout,
-                'full_text': ""
+                'full_text': ''
             }
-
         return response
+
 
 if __name__ == "__main__":
     from time import sleep
--- py3status/modules/static_string.py
+++ py3status/modules/static_string.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Display static text
-
-Display static text given by "format" parameter
+Display static text.
 
 Configuration parameters:
-    - text          : text that should be printed
-    - separator     : whether the separator is shown or not (True or False)
-    - format        : text that should be printed
     - color         : color of printed text
-    - on_click      : read goo.gl/u10n0x
+    - format        : text that should be printed
+    - separator     : whether the separator is shown or not (true or false)
 
 @author frimdo ztracenastopa@centrum.cz
 """
 
+from time import time
+
 
 class Py3status:
+    """
+    """
+    # available configuration parameters
     color = None
     format = ''
     separator = True
 
     def static_string(self, i3s_output_list, i3s_config):
-
         response = {
-            'full_text': self.format,
+            'cached_until': time() + 60,
             'color': self.color,
+            'full_text': self.format,
             'separator': self.separator
-            }
-
+        }
         return response
+
 
 if __name__ == "__main__":
     x = Py3status()
