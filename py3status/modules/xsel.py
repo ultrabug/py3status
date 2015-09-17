f0c5a455ac073624347fe8b076b60c02b488be80
Ultrabug <ultrabug@gentoo.org>
2015-09-17 09:36:18 +0200

    xsel module: little QA and fix python3 support

--- py3status/modules/xsel.py
+++ py3status/modules/xsel.py
@@ -3,15 +3,15 @@
 Display the X selection.
 
 Configuration parameters:
-   cache_timeout: how often we refresh this module in seconds
+    cache_timeout: how often we refresh this module in seconds
                    (default is at every py3status configured interval)
-   command: the xsel command to run (default 'xsel')
-   max_size: stip the selection to this value (default 15)
-   symmetric: show the beginning and the end of the selection string
+    command: the xsel command to run (default 'xsel')
+    max_size: stip the selection to this value (default 15)
+    symmetric: show the beginning and the end of the selection string
                with respect to configured max_size.
 
 Requires:
-   - xsel command line tool
+    - xsel command line tool
 
 @author Sublim3 umbsublime@gamil.com
 @license BSD
@@ -25,7 +25,7 @@ from subprocess import check_output
 
 class Py3status:
     """
-   """
+    """
     # available configuration parameters
     cache_timeout = 0.5
     command = 'xsel'
@@ -35,8 +35,8 @@ class Py3status:
 
     def xsel(self, i3s_output_list, i3s_config):
         """
-       Display the content of xsel.
-       """
+        Display the content of xsel.
+        """
         current_value = check_output(shlex.split(self.command))
         if len(current_value) >= self.max_size:
             if self.symmetric is True:
@@ -47,7 +47,7 @@ class Py3status:
                 current_value = current_value[:self.max_size]
         response = {
             'cached_until': time() + self.cache_timeout,
-            'full_text': current_value.decode('utf-8'),
+            'full_text': current_value,
             'color': self.color
         }
         return response
@@ -55,8 +55,8 @@ class Py3status:
 
 if __name__ == "__main__":
     """
-   Test this module by calling it directly.
-   """
+    Test this module by calling it directly.
+    """
     from time import sleep
     x = Py3status()
     config = {
