16252467d3395008e524211dba1540ddf31c4e28
Brian Stinson <brian@bstinson.com>
2015-12-07 23:29:46 -0600

    Add a module that reports the current selinux state

--- py3status/modules/README.md
+++ py3status/modules/README.md
@@ -472,6 +472,21 @@ Available modules:
                          @author shadowprince
                          @license Eclipse Public License
                          ---
+  selinux_state          Display the current selinux state.
+  
+                         This module displays the current state of selinux on your machine: Enforcing
+                         (good), Permissive (bad), or Disabled (bad).
+
+                         Configuration parameters:
+                             - cache_timeout : how often we refresh this module in seconds (10s default)
+
+                         Requires:
+                             - libselinux-python
+                             - libselinux-pyton3 (optional for python3 support)
+
+                         @author bstinsonmhk
+                         @license MIT
+                         ---
   screenshot             Take a screenshot and optionally upload it to your online server.
                          
                          Display a 'SHOT' button in your i3bar allowing you to take a screenshot and
---
+++ py3status/modules/selinux_state.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Display the current selinux state.
+
+This module displays the current state of selinux on your machine: Enforcing
+(good), Permissive (bad), or Disabled (bad).
+
+Configuration parameters:
+    - cache_timeout : how often we refresh this module in seconds (10s default)
+
+Requires:
+    - libselinux-python
+    - libselinux-python3 (optional for python3 support)
+
+@author bstinsonmhk
+@license MIT
+"""
+
+import selinux
+from time import time
+
+
+class Py3status:
+    """
+    """
+
+    cache_timeout = 10
+
+    def __init__(self):
+        pass
+
+    def kill(self, i3s_output_list, i3s_config):
+        pass
+
+    def on_click(self, i3s_output_list, i3s_config):
+        pass
+
+    def selinux_status(self, i3s_output_list, i3s_config):
+
+        try:
+            if selinux.security_getenforce():
+                selinuxstring = 'enforcing'
+                color = 'color_good'
+            else:
+                selinuxstring = 'permissive'
+                color = 'color_bad'
+        except OSError:
+            selinuxstring = 'disabled'
+            color = 'color_bad'
+
+        response = {
+            'cached_until': time() + self.cache_timeout,
+            'full_text': 'selinux: {0}'.format(selinuxstring),
+            'color': i3s_config[color]
+        }
+
+        return response
+
+if __name__ == '__main__':
+    from time import sleep
+
+    x = Py3status()
+
+    config = {
+        'color_good': '#00FF00',
+        'color_bad': '#FF0000',
+    }
+
+    while True:
+        print(x.selinux_status([], config))
+        sleep(1)
