a7a6d8202839830626d3f789774705df7e57930f
Ultrabug <ultrabug@gentoo.org>
2016-02-07 19:40:53 +0100

    icinga2 module: qa and code cleanup

--- py3status/modules/icinga2.py
+++ py3status/modules/icinga2.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Display Icinga2 service status information
+Display Icinga2 service status information.
 
 Configuration Parameters:
-    - cache_timeout: how often the data should be updated
     - base_url: the base url to the icinga-web2 services list
-    - disable_acknowledge: enable or disable counting of acknowledged service problems
-    - user: username to authenticate against the icinga-web2 interface
-    - password: password to authenticate against the icinga-web2 interface
-    - format: define a format string like "CRITICAL: %d"
+    - cache_timeout: how often the data should be updated
     - color: define a color for the output
+    - disable_acknowledge: enable or disable counting of acknowledged service problems
+    - format: define a format string like "CRITICAL: %d"
+    - password: password to authenticate against the icinga-web2 interface
     - status: set the status you want to optain (0=OK,1=WARNING,2=CRITICAL,3=UNKNOWN)
+    - user: username to authenticate against the icinga-web2 interface
 
 @author Ben Oswald <ben.oswald@root-space.de>
 @license BSD License <https://opensource.org/licenses/BSD-2-Clause>
@@ -19,35 +19,31 @@ Configuration Parameters:
 from time import time
 import requests
 
+STATUS_NAMES = {0: 'OK', 1: 'WARNING', 2: 'CRITICAL', 3: 'UNKNOWN'}
+
+
 class Py3status:
     """
     """
-    STATUS_NAMES = {
-            0: 'OK',
-            1: 'WARNING',
-            2: 'CRITICAL',
-            3: 'UNKNOWN'
-            }
     # available configuration parameters
-    cache_timeout = 60
     base_url = ''
+    ca = True
+    cache_timeout = 60
+    color = '#ffffff'
     disable_acknowledge = False
+    format = '{status_name}: {count}'
+    password = ''
+    status = 0
     url_parameters = "?service_state={service_state}&format=json"
     user = ''
-    password = ''
-    ca = True
-    format = '{status_name}: {count}'
-    color = '#ffffff'
-    status = 0
 
     def get_status(self, i3s_output_list, i3s_config):
         response = {
             'color': self.color,
             'cached_until': time() + self.cache_timeout,
             'full_text': self.format.format(
-                status_name=self.STATUS_NAMES.get(self.status, "INVALID STATUS"),
-                count=self._query_service_count(self.status)
-                )
+                status_name=STATUS_NAMES.get(self.status, "INVALID STATUS"),
+                count=self._query_service_count(self.status))
         }
         return response
 
@@ -57,8 +53,19 @@ class Py3status:
             url_parameters = url_parameters + "&service_handled=0"
         result = requests.get(
             self.base_url + url_parameters.format(service_state=state),
-            auth=(self.user, self.password), verify=self.ca)
+            auth=(self.user, self.password),
+            verify=self.ca)
         return len(result.json())
 
+
 if __name__ == "__main__":
-    pass
+    from time import sleep
+    x = Py3status()
+    config = {
+        'color_bad': '#FF0000',
+        'color_degraded': '#FFFF00',
+        'color_good': '#00FF00'
+    }
+    while True:
+        print(x.get_status([], config)['full_text'])
+        sleep(1)
