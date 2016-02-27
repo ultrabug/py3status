6ce135f0e4965f2eca8b85b674cf624b7e203c84
Themistokle Benetatos <info@mrt-prodz.com>
2016-02-27 22:54:08 +0200

    deadbeef module to show current track playing

---
+++ py3status/modules/deadbeef.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Display track currently playing in deadbeef.
+
+Configuration parameters:
+    - cache_timeout : how often we refresh usage in seconds (1s default)
+    - format : see placeholders below
+
+Format of status string placeholders:
+    {artist} : artist
+    {title} : title
+    {elapsed} : elapsed time
+    {length} : total length
+    {year} : year
+    {tracknum} : track number
+
+@author mrt-prodz
+"""
+from subprocess import check_output
+from time import time
+
+class Py3status:
+    # available configuration parameters
+    cache_timeout = 1
+    artist = ''
+    title = ''
+    length = ''
+    elapsed = ''
+    year = ''
+    format = '{artist} - {title}'
+
+    # return error occurs
+    def _error_response(self, color):
+        response = {
+            'cached_until': time() + self.cache_timeout,
+            'full_text' : 'deadbeef: error',
+            'color' : color
+        }
+        return response
+
+
+    # return track currently playing in deadbeef
+    def get_status(self, i3s_output_list, i3s_config):
+        try:
+            # get all properties using ¥ as delimiter
+            status = check_output(['deadbeef', '--nowplaying', '%a¥%t¥%l¥%e¥%y¥n'])
+            # check if we have music currently  playing
+            if 'nothing' in status:
+                response = {
+                    'cached_until': time() + self.cache_timeout,
+                    'full_text' : ''
+                }
+                return response
+            # split properties using special delimiter
+            parts =  status.split('¥')
+            if len(parts) == 6:
+                self.artist = parts[0]
+                self.title = parts[1]
+                self.length = parts[2]
+                self.elapsed = parts[3]
+                self.year = parts[4]
+                self.tracknum = parts[5]
+            else:
+                return self._error_response(i3s_config['color_bad'])
+
+            response = {
+                'cached_until': time() + self.cache_timeout,
+                'full_text' : self.format.format(artist=self.artist,
+                                                 title=self.title,
+                                                 length=self.length,
+                                                 elapsed=self.elapsed,
+                                                 year=self.year,
+                                                 tracknum=self.tracknum)
+            }
+            return response
+        except:
+            return self._error_response(i3s_config['color_bad'])
+
+if __name__ == "__main__":
+    """
+    Test this module by calling it directly.
+    """
+    x = Py3status()
+    config = {
+        'color_good': '#00FF00',
+        'color_degraded': '#00FFFF',
+        'color_bad': '#FF0000'
+    }
+    print(x.get_status([], config))
