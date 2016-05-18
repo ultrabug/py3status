22faca48a214d4116b508b2841087bf34bd1dd61
cypher <jp10010101010000@gmail.com>
2016-05-18 10:03:52 +1000

    Added the number of queued files to the sync status

--- py3status/modules/insync.py
+++ py3status/modules/insync.py
@@ -5,11 +5,12 @@ Gets the current insync status
 
 Configuration parameters:
     format: Display format to use *(default
-        '{status}'
+        '{status} {queued}'
         )*
 
 Format status string parameters:
     {status} Status of Insync
+    {queued} Number of files queued
 
 @author Joshua Pratt <jp10010101010000@gmail.com>
 Thanks to @author Iain Tatch <iain.tatch@gmail.com> for the script that this is based on
@@ -23,7 +24,7 @@ import subprocess
 class Py3status:
     # available configuration parameters
     cache_timeout = 1
-    format = '{status}'
+    format = '{status} {queued}'
 
     def check_insync(self, i3s_output_list, i3s_config):
         status = str(subprocess.check_output(["/usr/bin/insync", "get_status"]))
@@ -35,7 +36,15 @@ class Py3status:
         if status == "SHARE":
             color = i3s_config.get('color_good', '')
             status = "INSYNC"
-        results = self.format.format(status=str(status))
+
+        queued = str(subprocess.check_output(["/usr/bin/insync", "get_sync_progress"]))
+        queued = queued.split("\\n")
+        if len(queued) > 2 and "queued" in queued[-2]:
+            queued = queued[-2]
+            queued = queued.split(" ")[0].replace("b'", "")
+        else:
+            queued = ""
+        results = self.format.format(status=status, queued=queued)
 
         response = {
             'cached_until': time() + self.cache_timeout,
