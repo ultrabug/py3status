2c5b67d61acff1d3b76cda3a41e2c1aac65e71d0
Ultrabug <ultrabug@gentoo.org>
2016-08-16 16:27:26 +0200

    check_tcp module: QA and use latest py3 functionalities

--- py3status/modules/check_tcp.py
+++ py3status/modules/check_tcp.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Display if a ssh on host is available
+Display if a TCP port is available on the given host.
 
 Configuration parameters:
-    cache_timeout: how often to run the check
+    cache_timeout: how often to run the check, default 10s
     color_down: color of format_down
     color_up: color of format_up
     format_down: what to display when tcp port is down
     format_up: what to display when tcp port is up
-    host: check if tcp port on host is up
-    port: the tcp port
+    host: check if tcp port on host is up, default localhost
+    port: the tcp port, default 22
 
 @author obb, Moritz Lüdecke
 """
@@ -25,27 +25,32 @@ class Py3status:
     cache_timeout = 10
     color_down = None
     color_up = None
-    format_down = 'TCP port down'
-    format_up = 'TCP port up'
+    format_down = '{host}:{port} is down'
+    format_up = '{host}:{port} is up'
     host = 'localhost'
     port = 22
 
     def check_tcp(self, i3s_output_list, i3s_config):
         response = {
-            'cached_until': time() + self.cache_timeout
+            'cached_until': self.py3.time_in(self.cache_timeout)
         }
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((self.host, self.port))
 
         if result == 0:
-            response['full_text'] = self.format_up
-            response['color'] = self.color_up or i3s_config['color_good']
+            response['full_text'] = self.py3.safe_format(self.format_up,
+                                                         {'host': self.host,
+                                                          'port': self.port})
+            response['color'] = self.color_up or self.py3.COLOR_GOOD
         else:
-            response['full_text'] = self.format_down
-            response['color'] = self.color_down or i3s_config['color_bad']
+            response['full_text'] = self.py3.safe_format(self.format_down,
+                                                         {'host': self.host,
+                                                          'port': self.port})
+            response['color'] = self.color_down or self.py3.COLOR_BAD
 
         return response
+
 
 if __name__ == "__main__":
     """
