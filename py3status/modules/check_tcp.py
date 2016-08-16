305be3e7bb70ba3673db3261232aa841cbf17f61
Ultrabug <ultrabug@gentoo.org>
2016-08-16 17:35:14 +0200

    check_tcp module: use a single format option

--- py3status/modules/check_tcp.py
+++ py3status/modules/check_tcp.py
@@ -6,10 +6,12 @@ Configuration parameters:
     cache_timeout: how often to run the check (default 10)
     color_down: color of format_down
     color_up: color of format_up
-    format_down: what to display when tcp port is down
-    format_up: what to display when tcp port is up
-    host: check if tcp port on host is up (default localhost)
+    format: what to display on the bar (default '{host}:{port} {state}')
+    host: check if tcp port on host is up (default 'localhost')
     port: the tcp port (default 22)
+
+Format of status string placeholders:
+    {state} port state ('DOWN' or 'UP')
 
 @author obb, Moritz Lüdecke
 """
@@ -24,30 +26,27 @@ class Py3status:
     cache_timeout = 10
     color_down = None
     color_up = None
-    format_down = '{host}:{port} is down'
-    format_up = '{host}:{port} is up'
+    format = '{host}:{port} {state}'
     host = 'localhost'
     port = 22
 
     def check_tcp(self, i3s_output_list, i3s_config):
-        response = {
-            'cached_until': self.py3.time_in(self.cache_timeout)
-        }
+        response = {'cached_until': self.py3.time_in(self.cache_timeout)}
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((self.host, self.port))
 
         if result == 0:
-            response['full_text'] = self.py3.safe_format(self.format_up,
-                                                         {'host': self.host,
-                                                          'port': self.port})
+            state = 'UP'
             response['color'] = self.color_up or self.py3.COLOR_GOOD
         else:
-            response['full_text'] = self.py3.safe_format(self.format_down,
-                                                         {'host': self.host,
-                                                          'port': self.port})
+            state = 'DOWN'
             response['color'] = self.color_down or self.py3.COLOR_BAD
 
+        response['full_text'] = self.py3.safe_format(self.format,
+                                                     {'host': self.host,
+                                                      'port': self.port,
+                                                      'state': state})
         return response
 
 
