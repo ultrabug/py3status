d62345e8453b371935d48438a98b924c2a4a739c
Guillaume Brogi <gui-gui@netcourrier.com>
2016-08-27 16:48:51 +0200

    Add option to sys_cpu

--- py3status/modules/sys_cpu.py
+++ py3status/modules/sys_cpu.py
@@ -4,27 +4,30 @@ Display the percent CPU used.
 This modules requires the psutil python module.
 
 Configuration parameters:
+    format: format string to use
+        (default: "☢{cpu:3d}%")
     threshold_degraded / threshold_bad: thresholds for color change
+
+Format status string parameters:
+    {cpu} cpu usage in percent
 """
 
 # import your useful libs here
-from time import time
 import psutil
 
 
 class Py3status:
-    # available configuration parameters
-    threshold_degraded = 50
-    threshold_bad = 80
-    cache_timeout = 1
-
     def __init__(self):
+        self.threshold_degraded = 50
+        self.threshold_bad = 80
+        self.cache_timeout = 1
+        self.format = "☢{cpu:3d}%"
         pass
 
     def cpu_percent(self, i3s_output_list, i3s_config):
         cpu = round(psutil.cpu_percent())
         response = {
-            'cached_until': time() + self.cache_timeout,
+            'cached_until': self.py3.time_in(seconds=self.cache_timeout),
             'full_text': ''
         }
         if cpu < self.threshold_degraded:
@@ -33,7 +36,7 @@ class Py3status:
             response['color'] = self.py3.COLOR_DEGRADED
         else:
             response['color'] = self.py3.COLOR_BAD
-        response['full_text'] = "☢{:3d}%".format(cpu)
+        response['full_text'] = self.py3.safe_format(self.format, {'cpu': cpu})
 
         return response
 
