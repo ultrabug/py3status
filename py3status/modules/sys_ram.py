12feff46b73d7330287f9ce916d13ae5587e5c80
Guillaume Brogi <gui-gui@netcourrier.com>
2016-08-27 17:13:00 +0200

    Add format string to RAM usage

--- py3status/modules/sys_ram.py
+++ py3status/modules/sys_ram.py
@@ -5,25 +5,28 @@ This modules requires the psutil python module.
 
 Configuration parameters:
     threshold_degraded / threshold_bad: thresholds for color change
+    format: format string
+        (defaut: "⚛{ram:3d}%")
+
+Format status string parameters:
+    {ram} ram usage in percent
 """
 
 # import your useful libs here
-from time import time
 import psutil
 
 
 class Py3status:
-    # available configuration parameters
-    threshold_degraded = 40
-    threshold_bad = 75
-    cache_timeout = 1
-
     def __init__(self):
+        self.threshold_degraded = 40
+        self.threshold_bad = 75
+        self.cache_timeout = 1
+        self.format = "⚛{ram:3d}%"
         pass
 
     def used_ram(self, i3s_output_list, i3s_config):
         response = {
-            'cached_until': time() + self.cache_timeout,
+            'cached_until': self.py3.time_in(seconds=self.cache_timeout),
             'full_text': ''
         }
         used_mem_percent = round(psutil.virtual_memory().percent)
@@ -33,7 +36,8 @@ class Py3status:
             response['color'] = self.py3.COLOR_DEGRADED
         else:
             response['color'] = self.py3.COLOR_BAD
-        response['full_text'] = "⚛{:3d}%".format(used_mem_percent)
+        response['full_text'] = self.py3.safe_format(self.format,
+                {'ram': used_mem_percent})
 
         return response
 
