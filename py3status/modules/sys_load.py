a2aaf8be777095cc0383324bc1c13dc510601207
Guillaume Brogi <gui-gui@netcourrier.com>
2016-08-27 17:03:27 +0200

    Add format string and parameters to sys_load

--- py3status/modules/sys_load.py
+++ py3status/modules/sys_load.py
@@ -4,26 +4,31 @@ Display the load average.
 
 Configuration parameters:
     threshold_degraded / threshold_bad: thresholds for color change
+    format: format string
+        (default: "{one:.2f} {five:.2f} {fifteen:.2f}")
+
+Format status string parameters:
+    {one} load average over the last minute
+    {five} load average over the last five minutes
+    {fifteen} load average over the last fifteen minutes
 """
 
 # import your useful libs here
-from time import time
 import os
 
 
 class Py3status:
-    # available configuration parameters
-    threshold_degraded = 2
-    threshold_bad = 4
-    cache_timeout = 5
-
     def __init__(self):
+        self.threshold_degraded = 2
+        self.threshold_bad = 4
+        self.cache_timeout = 5
+        self.format = "{one:.2f} {five:.2f} {fifteen:.2f}"
         pass
 
     def load(self, i3s_output_list, i3s_config):
         one, five, fifteen = os.getloadavg()
         response = {
-            'cached_until': time() + self.cache_timeout,
+            'cached_until': self.py3.time_in(seconds=self.cache_timeout),
             'full_text': ''
         }
         if one < self.threshold_degraded:
@@ -32,7 +37,11 @@ class Py3status:
             response['color'] = self.py3.COLOR_DEGRADED
         else:
             response['color'] = self.py3.COLOR_BAD
-        response['full_text'] = "{:.2f} {:.2f} {:.2f}".format(one, five, fifteen)
+        response['full_text'] = self.py3.safe_format(self.format, {
+            'one': one,
+            'five': five,
+            'fifteen': fifteen,
+            })
 
         return response
 
