110179296dd062d343536c2eadcfce8885814d58
Guillaume Brogi <gui-gui@netcourrier.com>
2016-08-27 16:31:09 +0200

    Add more paramters to cpu temp

--- py3status/modules/cpu_temp.py
+++ py3status/modules/cpu_temp.py
@@ -5,6 +5,12 @@ Display the CPU temperature
 Configuration parameters:
     zone: temperature zone to check.
         (default: 'coretemp-isa-0000')
+    format: format string
+        (default: '{temp:.1f}°C')
+    threshold_bad/threshold_degraded: threshold for changing the color
+
+Format status string parameters:
+    {temp} the temperature measured
 """
 
 # import your useful libs here
@@ -13,18 +19,17 @@ import subprocess
 
 
 class Py3status:
-    # available configuration parameters
-    cache_timeout = 10
-    threshold_degraded = 80
-    threshold_bad = 100
-    zone = 'coretemp-isa-0000'
-
     def __init__(self):
+        self.cache_timeout = 10
+        self.threshold_degraded = 80
+        self.threshold_bad = 100
+        self.zone = 'coretemp-isa-0000'
+        self.format = '{temp:.1f}°C'
         pass
 
     def cpu_temp(self, i3s_output_list, i3s_config):
         response = {
-            'cached_until': time() + self.cache_timeout,
+            'cached_until': self.py3.time_in(seconds=self.cache_timeout),
             'full_text': ''
         }
         temp = float(subprocess.check_output(['sensors',self.zone]).split()[7].decode("utf-8")[1:-2])
@@ -34,7 +39,7 @@ class Py3status:
             response['color'] = self.py3.COLOR_DEGRADED
         else:
             response['color'] = self.py3.COLOR_BAD
-        response['full_text'] = "{:.1f}°C".format(temp)
+        response['full_text'] = self.py3.safe_format(self.format, {'temp': temp})
 
         return response
 
