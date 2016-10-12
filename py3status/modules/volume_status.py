d059bb5f83cb486cb30d0268efbec1a9420e7af5
Guillaume Brogi <gui-gui@netcourrier.com>
2016-10-12 22:46:04 +0200

    Fix summary

--- py3status/modules/volume_status.py
+++ py3status/modules/volume_status.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Display current sound volume using amixer.
+Display current sound volume.
 
 Expands on the standard i3status volume module by adding color
 and percentage threshold settings.
