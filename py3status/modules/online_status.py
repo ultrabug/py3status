295383cd5da5aa1f9abad341482dad3aaa5b6fa5
lasers <lasers@users.noreply.github.com>
2017-01-28 07:46:56 -0600

    online_status: new state

--- py3status/modules/online_status.py
+++ py3status/modules/online_status.py
@@ -5,13 +5,14 @@ Determine if you have an Internet Connection.
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 10)
     format: display format for this module (default '{icon}')
-    icon_off: display icon when offline (default '■')
-    icon_on: display icon when online (default '●')
-    url: connect to this url to check the connection status
+    icon_off: show when connection is offline (default '■')
+    icon_on: show when connection is online (default '●')
+    timeout: how many seconds before we announce offline (default 2)
+    url: name of url to use when checking for a connection
          (default 'http://www.google.com')
 
 Format placeholders:
-    {icon} display current online status
+    {icon} status icon
 
 Color options:
     color_bad: Offline
@@ -23,9 +24,11 @@ Color options:
 try:
     # python 3
     from urllib.parse import urlsplit
+    from urllib.request import urlopen
 except ImportError:
     # python 2
     from urlparse import urlsplit
+    from urllib2 import urlopen
 
 
 class Py3status:
@@ -36,6 +39,7 @@ class Py3status:
     format = '{icon}'
     icon_off = u'■'
     icon_on = u'●'
+    timeout = 2
     url = 'http://www.google.com'
 
     class Meta:
@@ -52,37 +56,70 @@ class Py3status:
                     'msg': 'obsolete parameter use `icon_off`',
                 },
             ],
-            'remove': [
-                {
-                    'param': 'timeout',
-                    'msg': 'obsolete parameter',
-                },
-            ]
         }
 
     def post_config_hook(self):
         self.color_on = self.py3.COLOR_ON or self.py3.COLOR_GOOD
         self.color_off = self.py3.COLOR_OFF or self.py3.COLOR_BAD
-        if '://' in self.url:
-            self.url = urlsplit(self.url).netloc
+        self.use_ping = None
 
-    def _connection_present(self):
+        if '://' in self.url:
+            self.ping_url = urlsplit(self.url).netloc
+
+    def _ping(self):
         try:
-            self.py3.command_output(['ping', '-c', '1', self.url])
+            self.py3.command_output(['ping', '-c', '1', self.ping_url])
+            return True
         except:
             return False
-        else:
+
+    def _urlopen(self):
+        try:
+            urlopen(self.url, timeout=self.timeout)
             return True
+        except:
+            return False
 
     def online_status(self):
-        run = self._connection_present()
-        icon = self.icon_on if run else self.icon_off
+        icon = self.icon_off
+        color = self.color_off
+
+        if self.use_ping:
+            self.py3.log("if use_ping: was true.")
+            if self._ping():
+                self.py3.log("...if ping (in ping), ping was good.")
+                icon = self.icon_on
+                color = self.color_on
+                self.use_ping = True
+            else:
+                self.py3.log("... if ping (in ping) ping was bad.")
+                self.use_ping = False
+        elif self._urlopen():
+            self.py3.log("elif urlopen: was true.")
+            icon = self.icon_on
+            color = self.color_on
+            if self._ping():
+                self.py3.log("... if ping (in urlopen), ping was good.")
+                self.use_ping = True
+            else:
+                self.py3.log("... if ping (in urlopen), ping was bad.")
+                self.use_ping = False
+
+        if self.use_ping is None:
+            self.py3.log("if use_ping: was None.")
+            if self._ping():
+                self.py3.log("...if ping (in use_ping), ping was good.")
+                self.use_ping = True
+                icon = self.icon_on
+                color = self.color_on
+            else:
+                self.py3.log("...if ping (in use_ping), ping was bad.")
+                self.use_ping = False
 
         return {
             'cached_until': self.py3.time_in(self.cache_timeout),
             'full_text': self.py3.safe_format(self.format, {'icon': icon}),
-            'color': self.color_on if run else self.color_off
-        }
+            'color': color}
 
 
 if __name__ == "__main__":
