504a03d732468edc7aeb01fecf556c5dd72a92e0
lasers <lasers@users.noreply.github.com>
2017-02-03 03:03:53 -0600

    check_tcp: new state

--- py3status/modules/check_tcp.py
+++ py3status/modules/check_tcp.py
@@ -5,21 +5,20 @@ Display status of a TCP port on a given host.
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 10)
     format: display format for this module (default '{host}:{port} {state}')
-    host: check host (default 'localhost')
-    port: check port number (default 22)
-    string_down: show string when available (default 'UP')
-    string_up: show string when unavailable (default 'DOWN')
+    host: name of host to check for (default 'localhost')
+    icon_off: show this when unavailable (default 'DOWN')
+    icon_on: show this when available (default 'UP')
+    port: number of port to check for (default 22)
 
 Format placeholders:
     {state} port state
 
 Color options:
-    color_down: Unavailable, default color_bad
-    color_up: Available, default color_good
+    color_down: Closed, default to color_bad
+    color_up: Open, default to color_good
 
 @author obb, Moritz Lüdecke
 """
-
 import socket
 
 
@@ -30,9 +29,9 @@ class Py3status:
     cache_timeout = 10
     format = '{host}:{port} {state}'
     host = 'localhost'
+    icon_off = 'DOWN'
+    icon_on = 'UP'
     port = 22
-    string_down = 'DOWN'
-    string_up = 'UP'
 
     def post_config_hook(self):
         self.color_on = self.py3.COLOR_UP or self.py3.COLOR_GOOD
@@ -41,13 +40,17 @@ class Py3status:
     def check_tcp(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((self.host, self.port))
-        state = self.string_down if result else self.string_up
 
-        return {
-            'cached_until': self.py3.time_in(self.cache_timeout),
-            'full_text': self.py3.safe_format(self.format, {'state': state}),
-            'color': self.color_off if result else self.color_on
-        }
+        if result:
+            color = self.color_off
+            state = self.icon_off
+        else:
+            color = self.color_on
+            state = self.icon_on
+
+        return {'cached_until': self.py3.time_in(self.cache_timeout),
+                'full_text': self.py3.safe_format(self.format, {'state': state}),
+                'color': color}
 
 
 if __name__ == "__main__":
