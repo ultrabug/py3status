cd3ba67a058efeb3141fb4696e460bfd254b420c
lasers <lasers@users.noreply.github.com>
2017-02-03 04:22:02 -0600

    clementine: new state

--- py3status/modules/clementine.py
+++ py3status/modules/clementine.py
@@ -5,19 +5,22 @@ Display song currently playing in Clementine.
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 5)
     format: display format for this module (default '♫ {current}')
-    string_unavailable: unavailable format (default 'clementine: N/A')
 
 Format placeholders:
     {current} currently playing
 
 Requires:
-    clementine:
+    clementine: a modern music player and library organizer
+    qdbus: a communication-interface for qt-based applications
+        (may be part of qt5-tools)
 
 @author Francois LASSERRE <choiz@me.com>
 @license GNU GPL http://www.gnu.org/licenses/gpl.html
 """
-
 CMD = 'qdbus org.mpris.clementine /TrackList org.freedesktop.MediaPlayer'
+STRING_UNAVAILABLE = "clementine: isn't installed"
+STRING_ERROR = "clementine: isn't running"
+INTERNET_RADIO = 'Internet Radio'
 
 
 class Py3status:
@@ -26,50 +29,57 @@ class Py3status:
     # available configuration parameters
     cache_timeout = 5
     format = u'♫ {current}'
-    string_unavailable = 'clementine: N/A'
 
     def clementine(self):
         """
         Get current song metadata: "artist - title"
         """
+        if not self.py3.check_commands(['clementine']):
+            return {
+                'cached_until': self.py3.CACHE_FOREVER,
+                'color': self.py3.COLOR_BAD,
+                'full_text': STRING_UNAVAILABLE
+            }
+
         artist = lines = now_playing = title = ''
         internet_radio = False
-        response = {'cached_until': self.py3.time_in(self.cache_timeout)}
 
         try:
             track_id = self.py3.command_output(CMD + '.GetCurrentTrack')
             metadata = self.py3.command_output(CMD + '.GetMetadata {}'.format(track_id))
             lines = filter(None, metadata.splitlines())
         except:
-            response['color'] = self.py3.COLOR_BAD
-            response['full_text'] = self.string_unavailable
-        else:
-            if lines:
-                for item in lines:
-                    if item.find('artist:') != -1:
-                        artist = item[8:]
-                    if item.find('title:') != -1:
-                        title = item[7:]
+            return {
+                'cached_until': self.py3.time_in(self.cache_timeout),
+                'color': self.py3.COLOR_BAD,
+                'full_text': STRING_ERROR
+            }
 
-                if title.find('.wav') != -1 or title.find('.mp3') != -1:
-                    title = title[:-4]
-                if title.find('http') != -1:
-                    title = ''
-                    internet_radio = True
+        for item in lines:
+            if 'artist' in item:
+                artist = item[8:]
+            if 'title' in item:
+                title = item[7:]
 
-                if artist and title:
-                    now_playing = '{} - {}'.format(artist, title)
-                elif artist:
-                    now_playing = artist
-                elif title:
-                    now_playing = title
-                elif internet_radio:
-                    now_playing = 'Internet Radio'
+        if '.mp3' in title or '.wav' in title:
+            title = title[:-4]
+        if 'http' in title:
+            title = ''
+            internet_radio = True
 
-                response['full_text'] = self.py3.safe_format(
-                    self.format, {'current': now_playing}).strip()
+        if artist and title:
+            now_playing = u'{} - {}'.format(artist, title)
+        elif artist:
+            now_playing = artist
+        elif title:
+            now_playing = title
+        elif internet_radio:
+            now_playing = INTERNET_RADIO
 
-        return response
+        return {
+            'cached_until': self.py3.time_in(self.cache_timeout),
+            'full_text': self.py3.safe_format(self.format, {'current': now_playing})
+        }
 
 
 if __name__ == "__main__":
