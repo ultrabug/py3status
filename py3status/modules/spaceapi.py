ae4ada2ffe272359f480c28e70222f368f43a7d4
lasers <lasers@users.noreply.github.com>
2017-03-08 20:52:03 -0600

    spaceapi: add format

--- py3status/modules/spaceapi.py
+++ py3status/modules/spaceapi.py
@@ -3,26 +3,27 @@
 Display status of a given hackerspace.
 
 Configuration parameters:
-    button_url: Button that when clicked opens the URL sent in the space's API.
-        Setting to None disables. (default 3)
-    cache_timeout: Set timeout between calls in seconds (default 60)
-    closed_text: text if space is closed, strftime parameters
-        will be translated (default 'closed')
-    open_text: text if space is open, strftime parmeters will be translated
-        (default 'open')
-    time_text: format used for time display (default ' since %H:%M')
-    url: URL to SpaceAPI json file of your space
+    button_url: mouse button to open URL sent in space's API (default 3)
+    cache_timeout: refresh interval for this module (default 60)
+    format: display format for this module (default '{spaceapi}')
+    icon_closed: show when hackerspace is closed (default 'closed')
+    icon_open: show when hackerspace is open (default 'open')
+    icon_time: display format for time (default ' since %H:%M')
+    url: specify JSON URL of a hackerspace to retrieve from
         (default 'http://status.chaospott.de/status.json')
 
+__Note: Strftime parameters in icon_* parameters will be translated.__
+
 Color options:
-    color_closed: Space is open, defaults to color_bad
-    color_open: Space is closed, defaults to color_good
+    color_bad: Space closed
+    color_good: Space open
 
 @author timmszigat
 @license WTFPL <http://www.wtfpl.net/txt/copying/>
 """
 
 import datetime
+STRING_UNAVAILABLE = 'spaceapi: N/A'
 
 
 class Py3status:
@@ -31,9 +32,10 @@ class Py3status:
     # available configuration parameters
     button_url = 3
     cache_timeout = 60
-    closed_text = 'closed'
-    open_text = 'open'
-    time_text = ' since %H:%M'
+    format = '{spaceapi}'
+    icon_closed = 'closed'
+    icon_open = 'open'
+    icon_time = ' since %H:%M'
     url = 'http://status.chaospott.de/status.json'
 
     class Meta:
@@ -41,63 +43,72 @@ class Py3status:
             'rename': [
                 {
                     'param': 'open_color',
-                    'new': 'color_open',
-                    'msg': 'obsolete parameter use `color_open`',
+                    'new': 'color_good',
+                    'msg': 'obsolete parameter use `color_good`',
                 },
                 {
                     'param': 'closed_color',
-                    'new': 'color_closed',
-                    'msg': 'obsolete parameter use `color_closed`',
+                    'new': 'color_bad',
+                    'msg': 'obsolete parameter use `color_bad`',
+                },
+                {
+                    'param': 'color_open',
+                    'new': 'color_good',
+                    'msg': 'obsolete parameter use `color_good`',
+                },
+                {
+                    'param': 'color_closed',
+                    'new': 'color_bad',
+                    'msg': 'obsolete parameter use `color_bad`',
+                },
+                {
+                    'param': 'closed_text',
+                    'new': 'icon_closed',
+                    'msg': 'obsolete parameter use `icon_closed`',
+                },
+                {
+                    'param': 'open_text',
+                    'new': 'icon_open',
+                    'msg': 'obsolete parameter use `icon_open`',
+                },
+                {
+                    'param': 'time_text',
+                    'new': 'icon_time',
+                    'msg': 'obsolete parameter use `icon_time`',
                 },
             ],
         }
 
-    def check(self):
-
-        response = {
-            'cached_until': self.py3.time_in(self.cache_timeout),
-        }
+    def spaceapi(self):
+        color = self.py3.COLOR_BAD
+        full_text = self.icon_closed
 
         try:
             data = self.py3.request(self.url).json()
             self._url = data.get('url')
 
-            if(data['state']['open'] is True):
-                response['color'] = self.py3.COLOR_OPEN or self.py3.COLOR_GOOD
-                if 'lastchange' in data['state'].keys():
-                    response['full_text'] = self.open_text + self.time_text
-                    response['short_text'] = '%H:%M'
-                else:
-                    response['full_text'] = self.open_text
-                    response['short_text'] = self.open_text
-
-            else:
-                response['color'] = self.py3.COLOR_CLOSED or self.py3.COLOR_BAD
-                if 'lastchange' in data['state'].keys():
-                    response['full_text'] = self.closed_text + self.time_text
-                    response['short_text'] = ''
-                else:
-                    response['full_text'] = self.closed_text
-                    response['short_text'] = self.closed_text
+            if(data['state']['open']):
+                color = self.py3.COLOR_GOOD
+                full_text = self.icon_open
 
             if 'lastchange' in data['state'].keys():
-                # apply strftime to full and short text
-                dt = datetime.datetime.fromtimestamp(
-                    data['state']['lastchange']
-                )
-                response['full_text'] = dt.strftime(response['full_text'])
-                response['short_text'] = dt.strftime(response['short_text'])
+                full_text += self.icon_time
+                dt = datetime.datetime.fromtimestamp(data['state']['lastchange'])
+                full_text = dt.strftime(full_text)
 
+            full_text = self.py3.safe_format(self.format, {'spaceapi': full_text})
         except:
-            response['full_text'] = ''
-
-        return response
+            full_text = STRING_UNAVAILABLE
+        return {
+            'cached_until': self.py3.time_in(self.cache_timeout),
+            'full_text': full_text,
+            'color': color
+        }
 
     def on_click(self, event):
         button = event['button']
         if self._url and self.button_url == button:
-            cmd = 'xdg-open {}'.format(self._url)
-            self.py3.command_run(cmd)
+            self.py3.command_run('xdg-open {}'.format(self._url))
             self.py3.prevent_refresh()
 
 
