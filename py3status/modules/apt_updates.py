22c7f83bb24b3563c6af1597f580ecd70e2968ae
Michael Ilsaas <mikeri@mikeri.net>
2017-08-15 21:02:02 +0200

    Added options for format_one and format_none

--- py3status/modules/apt_updates.py
+++ py3status/modules/apt_updates.py
@@ -9,7 +9,13 @@ This will display a count of how many 'apt' updates are waiting to be installed.
 Configuration parameters:
     cache_timeout: How often we refresh this module in seconds (default 600)
     format: Display format to use
-        (default 'UPD[\?not_zero : {apt}]')
+        (default 'UPD: {apt}')
+    format_one: Display format to use if one update is available. If left empty,
+    default format will be used.
+        (default '')
+    format_none: Display format to use if no updates are available.
+        (default '')
+
 
 Format placeholders:
     {apt} Number of pending apt updates
@@ -34,19 +40,24 @@ class Py3status:
     # available configuration parameters
     cache_timeout = 600
     format = 'UPD[\?not_zero : {apt}]'
+    format_one = ''
+    format_none = ''
 
     def check_updates(self):
         apt_updates = self._check_apt_updates()
 
-        color = self.py3.COLOR_DEGRADED
         if apt_updates == 0:
             color = self.py3.COLOR_GOOD
-        full_text = self.py3.safe_format(
-            self.format,
-            {
-                'apt': apt_updates
-            }
-        )
+            full_text = self.py3.safe_format(
+                self.format_none, {'apt': apt_updates})
+        elif apt_updates == 1 and self.format_one:
+            color = self.py3.COLOR_DEGRADED
+            full_text = self.py3.safe_format(
+                self.format_one, {'apt': apt_updates})
+        else:
+            color = self.py3.COLOR_DEGRADED
+            full_text = self.py3.safe_format(
+                self.format, {'apt': apt_updates})
         return {
             'color': color,
             'cached_until': self.py3.time_in(self.cache_timeout),
