12760e5c5a565efa7dc86fe94a89de4c0f309d66
girst <girst@users.noreply.github.com>
2017-10-30 22:15:28 +0100

    merge imap_idle and imap; incorporate lasers' feedback

--- py3status/modules/imap.py
+++ py3status/modules/imap.py
@@ -14,6 +14,8 @@ Configuration parameters:
     security: login authentication method: 'ssl' or 'starttls'
         (startssl needs python 3.2 or later) (default 'ssl')
     server: server to connect (default None)
+    use_idle: use IMAP4 IDLE instead of polling; requires imaplib2, 
+        compatible server; disables cache_timeout (default: False)
     user: login user (default None)
 
 Format placeholders:
@@ -22,12 +24,18 @@ Format placeholders:
 Color options:
     color_new_mail: use color when new mail arrives, default to color_good
 
-@author obb
+@author obb and girst
 
 SAMPLE OUTPUT
 {'full_text': 'Mail: 36', 'color': '#00FF00'}
 """
-import imaplib
+try:
+    from imaplib2 import imaplib2 as imaplib
+    from threading import Thread
+    use_lib2 = True  # allow use_idle
+except ImportError:
+    import imaplib
+    use_lib2 = False
 from ssl import create_default_context
 STRING_UNAVAILABLE = 'N/A'
 
@@ -46,6 +54,7 @@ class Py3status:
     port = '993'
     security = 'ssl'
     server = None
+    use_idle = False
     user = None
 
     class Meta:
@@ -65,61 +74,80 @@ class Py3status:
         }
 
     def post_config_hook(self):
+        # class variables:
+        self.mail_count = 0
+        self.connection = None
+
         if self.security not in ["ssl", "starttls"]:
             raise ValueError("Unknown security protocol")
+        if not use_lib2 and self.use_idle:
+            raise ValueError("need imaplib2 for use_idle")
+        if self.use_idle:
+            self.idle_thread = Thread(target=self._get_mail_count)
+            self.idle_thread.start()
 
     def check_mail(self):
-        mail_count = self._get_mail_count()
+        if self.use_idle:
+            #TODO: check if thread is still running?
+            response = {'cached_until': self.py3.time_in(seconds=1)}
+        else:
+            self._get_mail_count()
+            response = {'cached_until': self.py3.time_in(self.cache_timeout)}
 
-        response = {'cached_until': self.py3.time_in(self.cache_timeout)}
-
-        if mail_count is None:
+        if self.mail_count is None:
             response['color'] = self.py3.COLOR_BAD,
             response['full_text'] = self.py3.safe_format(
                 self.format, {'unseen': STRING_UNAVAILABLE})
-        elif mail_count > 0:
+        elif self.mail_count > 0:
             response['color'] = self.py3.COLOR_NEW_MAIL or self.py3.COLOR_GOOD
             if self.allow_urgent:
                 response['urgent'] = True
 
-        if mail_count == 0 and self.hide_if_zero:
+        if self.mail_count == 0 and self.hide_if_zero:
             response['full_text'] = ''
         else:
-            response['full_text'] = self.py3.safe_format(self.format, {'unseen': mail_count})
+            response['full_text'] = self.py3.safe_format(self.format, {'unseen': self.mail_count})
 
         return response
 
     def _connection_ssl(self):
-        connection = imaplib.IMAP4_SSL(self.server, self.port)
+        connection = imaplib.IMAP4_SSL(self.server, int(self.port))
         return connection
 
     def _connection_starttls(self):
-        connection = imaplib.IMAP4(self.server, self.port)
+        connection = imaplib.IMAP4(self.server, int(self.port))
         connection.starttls(create_default_context())
         return connection
 
     def _get_mail_count(self):
         try:
-            mail_count = 0
-            directories = self.mailbox.split(',')
+            while True:
+                if self.connection is None:
+                    if self.security == "ssl":
+                        self.connection = self._connection_ssl()
+                    elif self.security == "starttls":
+                        self.connection = self._connection_starttls()
 
-            if self.security == "ssl":
-                connection = self._connection_ssl()
-            elif self.security == "starttls":
-                connection = self._connection_starttls()
+                    self.connection.login(self.user, self.password)
+                    
+                self.mail_count = 0
+                directories = self.mailbox.split(',')
 
-            connection.login(self.user, self.password)
-
-            for directory in directories:
-                connection.select(directory)
-                unseen_response = connection.search(None, self.criterion)
-                mails = unseen_response[1][0].split()
-                mail_count += len(mails)
-
-            connection.close()
-            return mail_count
-        except:
-            return None
+                for directory in directories:
+                    self.connection.select(directory)
+                    unseen_response = self.connection.search(None, self.criterion)
+                    mails = unseen_response[1][0].split()
+                    self.mail_count += len(mails)
+                    
+                if self.use_idle:
+                    self.connection.idle()
+                else:
+                    return
+        except Exception as e:
+            if self.connection is not None:
+                self.connection.close()
+                self.connection.logout()
+            self.connection = None
 
 
 if __name__ == "__main__":
