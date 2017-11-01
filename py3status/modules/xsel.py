91d06a1d4a710abe74a37a31cf51a0208a02f75e
Daniel Schäfer <github@danielschaefer.me>
2017-11-01 19:29:49 +0100

    Clear clipboard onclick and optional log (xsel) (#1088)
    * Clear clipboard onclick and optional log (xsel)
    
    * Correct spelling mistake
    
    * Improve config documentation
    
    * Merge whitespace without importing re
    
    * Handle click in config not in module
    
    * Initialize cache in hook and expand tilde in path
    
    * Add example for using logging
    
    * Move Example above sample output

--- py3status/modules/xsel.py
+++ py3status/modules/xsel.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+import time
+import os
 """
 Display X selection.
 
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 0.5)
-    command: the clipboard command to run (default 'xsel -o')
+    command: the clipboard command to run (default 'xsel --output')
     format: display format for this module (default '{selection}')
     max_size: strip the selection to this value (default 15)
     symmetric: show beginning and end of the selection string
         with respect to configured max_size. (default True)
+    log_file: specify the clipboard log to use (default None)
 
 Format placeholders:
     {selection} output from clipboard command
@@ -19,11 +22,16 @@ Requires:
 @author Sublim3 umbsublime@gamil.com
 @license BSD
 
+Example:
+xsel {
+  max_size = 50
+  command = "xsel --clipboard --output"
+  on_click 1 = "exec xsel --clear --clipboard"
+  log_file = "~/.local/share/xsel/clipboard_log"
+}
+
 SAMPLE OUTPUT
 {'full_text': 'selected text'}
-
-example
-{'full_text': 'rrrrr > wtf is a warlock doing in here'}
 """
 
 
@@ -32,13 +40,26 @@ class Py3status:
     """
     # available configuration parameters
     cache_timeout = 0.5
-    command = 'xsel -o'
+    command = 'xsel --output'
     format = '{selection}'
     max_size = 15
     symmetric = True
+    log_file = None
+
+    def post_config_hook(self):
+        self.selection_cache = None
+        self.log_file = os.path.expanduser(self.log_file)
 
     def xsel(self):
-        selection = ' '.join(self.py3.command_output(self.command).split())
+        selection = self.py3.command_output(self.command).strip()
+
+        if self.log_file and selection and selection != self.selection_cache:
+            with open(self.log_file, "a") as f:
+                datetime = time.strftime("%Y-%m-%d %H:%M:%S")
+                f.write("{}\n{}\n".format(datetime, selection))
+        self.selection_cache = selection
+
+        selection = ' '.join(selection.split())
         if len(selection) >= self.max_size:
             if self.symmetric is True:
                 split = int(self.max_size / 2) - 1
