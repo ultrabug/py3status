3b1c898bf4fb03b083c61542ae7fed00e5cba64d
Maikel Punie <maikel.punie@gmail.com>
2018-03-14 15:46:09 +0100

    made the module more generic

--- py3status/modules/mysql_slave.py
+++ py3status/modules/mysql_slave.py
@@ -6,15 +6,18 @@ This module displays the number of seconds the slave is behind the master.
 
 Configuration parameters:
     cache_timeout: refresh cache_timeout for this module (default 5)
-    format_master: format used for master_servers
-                    (default '[\\?color=seconds {host} is master]')
-    format_slave: format used for slave servers
-                    (default '[\\?color=seconds {host} is {seconds}s behind]')
+    format_fail: format used for master_servers
+      (default '[\?color=seconds_behind_master {host} is master]')
+    format_ok: format used for slave servers
+      (default '[\?color=seconds_behind_master {host} is {seconds_behind_master}s behind]')
     host: the host to connect to (default None)
     passwd: the password to use for the connection (default None)
     port: the port to connect to (default 3306)
+    query: The sql query to run (default 'show slave status')
     thresholds: a threshold list of tuples, -1 used for a master hosts
-      (default [(-1, 'blue'), (0, 'deepskyblue'), (100, 'good'), (300, 'degraded'), (600, 'bad')])
+        (default {'seconds_behind_master':
+            [(0, 'deepskyblue'), (100, 'good'), (300, 'degraded'), (600, 'bad')]
+        })
     user: the user used for the connection (default None)
 
 Requires:
@@ -38,44 +41,48 @@ class Py3status:
     """
     # available configuration parameters
     cache_timeout = 5
-    format_master = '[\?color=seconds {host} is master]'
-    format_slave = '[\?color=seconds {host} is {seconds}s behind]'
+    format_fail = '[\?color=seconds_behind_master {host} is master]'
+    format_ok = '[\?color=seconds_behind_master {host} is {seconds_behind_master}s behind]'
     host = None
     passwd = None
     port = 3306
-    thresholds = [
-            (-1, 'blue'),
+    query = 'show slave status'
+    thresholds = {
+        'seconds_behind_master': [
             (0, 'deepskyblue'),
             (100, 'good'),
             (300, 'degraded'),
             (600, 'bad')
         ]
+    }
     user = None
+
+    def _get_mysql_data(self):
+        '''Returns a dictionary of the query output.'''
+        conn = connect(user=self.user, passwd=self.passwd, host=self.host, port=self.port)
+        cur = conn.cursor()
+        cur.execute(self.query)
+        keys = [desc[0].lower() for desc in cur.description]
+        values = [str(val) for val in cur.fetchone()]
+        conn.close()
+        return dict(zip(keys, values))
 
     def mysql_slave(self):
         try:
-            data = self._get_slave_status()
-            seconds = data['Seconds_Behind_Master']
-            form = self.format_slave
+            data = self._get_mysql_data()
+            for k, v in data.items():
+                self.py3.threshold_get_color(data[k], k)
+            form = self.format_ok
         except:
-            form = self.format_master
-            seconds = -1
-        self.py3.threshold_get_color(seconds, 'seconds')
+            data = {}
+            form = self.format_fail
+
+        data['host'] = self.host
 
         return {
             'cached_until': self.py3.time_in(self.cache_timeout),
-            'full_text': self.py3.safe_format(form, {'seconds': str(seconds), 'host': self.host})
+            'full_text': self.py3.safe_format(form, data)
         }
-
-    def _get_slave_status(self):
-        '''Returns a dictionary of the 'SHOW SLAVE STATUS;' command output.'''
-        conn = connect(user=self.user, passwd=self.passwd, host=self.host, port=self.port)
-        cur = conn.cursor()
-        cur.execute('''SHOW SLAVE STATUS;''')
-        keys = [desc[0] for desc in cur.description]
-        values = cur.fetchone()
-        conn.close()
-        return dict(zip(keys, values))
 
 
 if __name__ == '__main__':
