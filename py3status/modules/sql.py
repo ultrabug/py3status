030573c158255c23a3add656651b08f191678e43
Maikel Punie <maikel.punie@gmail.com>
2018-03-21 07:45:14 +0100

    Hopefuly the last iteration ...

--- py3status/modules/sql.py
+++ py3status/modules/sql.py
@@ -1,93 +1,132 @@
 # -*- coding: utf-8 -*-
 """
-Display MySQL/MariaDB slave state.
-
-This module displays the number of seconds the slave is behind the master.
+Display data stored in MariaDB, MySQL, sqlite3, and hopefully more.
 
 Configuration parameters:
-    cache_timeout: refresh cache_timeout for this module (default 5)
-    database: The type of module where we need to call the conenct method from
-        (default 'MySQLdb')
-    format: display format for this module
-        *(default '\?color=seconds_behind_master {host} is '
-              '[{seconds_behind_master}s behind|\?show master]')*
-    parameters: The parameters needed for the connect function
-        (default {'user': None, 'port': None, 'passwd': None, 'host': None})
-    query: The sql query to run (default 'show slave status')
-    thresholds: a threshold list of tuples, -1 used for a master hosts
-        (default {'seconds_behind_master':
-            [(0, 'deepskyblue'), (100, 'good'), (300, 'degraded'), (600, 'bad')]
-        })
+    cache_timeout: refresh cache_timeout for this module (default 10)
+    database: specify a database name to import (default None)
+    format: display format for this module (default None)
+    parameters: specify connection parameters to use (default None)
+    query: specify a command to query on the database (default None)
+    thresholds: specify color thresholds to use (default [])
 
 Requires:
-    MySQLdb: python package for python
+    mariadb: fast sql database server, drop-in replacement for mysql
+    mysql-python: mysql support for python
+    sqlite: a c library that implements an sql database engine
+
+Examples:
+```
+# specify a database name to import
+sql {
+    database = 'sqlite3'  # from sqlite3 import connect
+    database = 'MySQLdb'  # from MySQLdb import connect
+    database = '...'      # from ... import connect
+}
+
+# specify connection parameters to use
+http://mysql-python.sourceforge.net/MySQLdb.html#functions-and-attributes
+https://docs.python.org/3/library/sqlite3.html#module-functions-and-constants
+sql {
+    name = 'MySQLdb'
+    parameters = {
+        'host': 'host', 'passwd': 'password', ...
+    }
+}
+
+# specify a command to query on the database
+sql {
+    query = 'SHOW SLAVE STATUS'
+    query = 'SELECT * FROM cal_todos'
+    query = '...'
+}
+
+# display number of seconds behind master with MySQLdb
+sql {
+    database = 'MySQLdb'
+    format += '[{seconds_behind_master}s behind|\?show master]'
+    format = '\?color=seconds_behind_master {host} is '
+    parameters = {'host': 'localhost', 'passwd': '********'}
+    query = 'SHOW SLAVE STATUS'
+    thresholds = [
+        (0, 'deepskyblue'), (100, 'good'), (300, 'degraded'), (600, 'bad')
+    ]
+}
+
+# query todo title with sqlite3
+sql {
+    database = 'sqlite3'
+    format = '{title}'
+    query = 'SELECT * FROM cal_todos'
+    parameters = '~/.thunderbird/user.default/calendar-data/local.sqlite'
+}
+```
+
+
 
 @author cereal2nd
 @license BSD
 
 SAMPLE OUTPUT
-{'full_text': 'test is 0s behind'}
-{'full_text': 'test is 509s behind'}
-{'full_text': 'test is master'}
+{'full_text': '509 seconds behind master'}
+{'full_text': 'master'}
 """
 
 from importlib import import_module
+from os.path import expanduser
 
 
 class Py3status:
     """
     """
     # available configuration parameters
-    cache_timeout = 5
-    database = 'MySQLdb'
-    format = ('\?color=seconds_behind_master {host} is '
-              '[{seconds_behind_master}s behind|\?show master]')
-    parameters = {
-        'host': None,
-        'user': None,
-        'passwd': None,
-        'port': None,
-    }
-    query = 'show slave status'
-    thresholds = {
-        'seconds_behind_master': [
-            (0, 'deepskyblue'),
-            (100, 'good'),
-            (300, 'degraded'),
-            (600, 'bad')
-        ]
-    }
+    cache_timeout = 10
+    database = None
+    format = None
+    parameters = None
+    query = None
+    thresholds = []
 
     def post_config_hook(self):
-        if not self.database:
-            raise Exception('Missing database')
-        if not self.parameters:
-            raise Exception('Missing parameters')
+        for config_name in ['database', 'format', 'parameters', 'query']:
+            if not getattr(self, config_name, None):
+                raise Exception('missing %s' % config_name)
         self.connect = getattr(import_module(self.database), 'connect')
+        self.is_param_a_string = isinstance(self.parameters, str)
+        if self.is_param_a_string:
+            self.parameters = expanduser(self.parameters)
 
     def _get_sql_data(self):
-        '''Returns a dictionary of the query output.'''
-        conn = self.connect(**self.parameters)
+        if self.is_param_a_string:
+            conn = self.connect(self.parameters)
+        else:
+            conn = self.connect(**self.parameters)
         cur = conn.cursor()
         cur.execute(self.query)
         keys = [desc[0].lower() for desc in cur.description]
-        values = [str(val) for val in cur.fetchone()]
+        values = [format(val) for val in cur.fetchone()]
         conn.close()
         return dict(zip(keys, values))
 
     def sql(self):
+        sql_data = {}
         try:
-            data = self._get_sql_data()
+            sql_data = self._get_sql_data()
         except:
-            data = {}
-        data['host'] = self.parameters['host']
+            pass
 
-        for k, v in data.items():
-            self.py3.threshold_get_color(data[k], k)
+        for k, v in sql_data.items():
+            self.py3.threshold_get_color(v, k)
+
+        if self.is_param_a_string:
+            merged_sql_data = sql_data
+        else:
+            merged_sql_data = self.parameters.copy()
+            merged_sql_data.update(sql_data)
 
         return {
             'cached_until': self.py3.time_in(self.cache_timeout),
-            'full_text': self.py3.safe_format(self.format, data)
+            'full_text': self.py3.safe_format(self.format, merged_sql_data)
         }
 
 
