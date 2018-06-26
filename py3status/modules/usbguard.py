edc000cb3c220f65f79402c4b1851433aab21fbf
Cyril Levis <levis.cyril@gmail.com>
2018-06-26 08:44:41 +0200

    first review of maximbaz and lasers urgent by default, use global option on click 1 allow and 2 for reject for the moment

--- py3status/modules/usbguard.py
+++ py3status/modules/usbguard.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Usbguard i3 companion to replace default 'usbguard-applet-qt'.
+USBGuard i3 companion to replace default 'usbguard-applet-qt'.
 Show an indicator when a new usb device is plugged,
 we can allow or reject a device.
 
 Configuration parameters:
-    allow_urgent: Allow urgent (default False)
     button_allow: Button to allow the device. (default 1)
     button_block: Button to block the device (default None)
-    button_reject: Button to reject the device. (default 3)
-    format: Display format for the module. (default '[UsbGuard: [{device_name}]]')
+    button_reject: Button to reject the device. (default 2)
+    format: Display format for the module. (default '[USBGuard: [{device_name}]]')
 
 Format placeholders:
     {device_name} the device name of last device plugged.
@@ -17,15 +16,13 @@ Format placeholders:
     {device_hash} the usbguard device unique hash of last device plugged.
 
 Requires:
-    https://usbguard.github.io:
-    "The USBGuard software framework helps to protect your
-    computer against rogue USB devices..."
+    usbguard: usb device authorization policy framework
 
 @author Cyril Levis (@cyrinux)
 @license BSD
 
 SAMPLE OUTPUT
-{'full_text': 'Usbguard: Mass Storage', 'urgent': True}
+{'full_text': 'USBGuard: Mass Storage', 'urgent': True}
 """
 
 import threading
@@ -33,27 +30,28 @@ from time import sleep
 from gi.repository import GLib
 from pydbus import SystemBus
 
-DBUS_INTERFACE = 'org.usbguard'
-DBUS_DEVICE_FILTER = '/org/usbguard/Devices'
+STRING_USBGUARD_DBUS = 'usbguard-dbus service not running'
 
 
 class UsbguardListener(threading.Thread):
+    """
+    """
+
     def __init__(self, parent):
         super(UsbguardListener, self).__init__()
         self.parent = parent
 
     def _setup_dbus(self):
         self.dbus = SystemBus()
-        self.dbus_devices_filter = DBUS_DEVICE_FILTER
         try:
             self.parent.error = None
-            self.proxy = self.dbus.get(DBUS_INTERFACE)
+            self.proxy = self.dbus.get(self.parent.dbus_interface)
             self.proxy_devices = self.proxy[".Devices"]
         except:
-            self.parent.error = Exception("usbguard-dbus service not running")
+            self.parent.error = Exception(STRING_USBGUARD_DBUS)
 
     # on device change signal
-    def _cb_devices_presence_changed(self, *args):
+    def _on_devices_presence_changed(self, *args):
         device = self.parent.device
         device_args = args[4]
         device_hash = device_args[4]['hash']
@@ -65,8 +63,7 @@ class UsbguardListener(threading.Thread):
         device['device_id'] = None
         device['device_hash'] = None
 
-        # if device inserted (1), removed (3)
-        if device_status == 1:
+        if device_status == 1:  # 1 inserted, 3 removed
             device['device_hash'] = device_hash
             device['device_id'] = device_id
             device['device_name'] = device_name
@@ -75,7 +72,7 @@ class UsbguardListener(threading.Thread):
         self.parent.py3.update()
 
     # on policy change signal
-    def _cb_devices_policy_changed(self, *args):
+    def _on_devices_policy_changed(self, *args):
         device = self.parent.device
         device_args = args[4]
         device_perms = device_args[3]
@@ -93,26 +90,26 @@ class UsbguardListener(threading.Thread):
         while not self.parent.killed.is_set():
             self._setup_dbus()
             self.dbus.subscribe(
-                object=self.dbus_devices_filter,
+                object=self.parent.dbus_devices,
                 signal='DevicePresenceChanged',
-                signal_fired=self._cb_devices_presence_changed)
+                signal_fired=self._on_devices_presence_changed)
 
             self.dbus.subscribe(
-                object=self.dbus_devices_filter,
+                object=self.parent.dbus_devices,
                 signal='DevicePolicyChanged',
-                signal_fired=self._cb_devices_policy_changed)
+                signal_fired=self._on_devices_policy_changed)
 
             self.loop = GLib.MainLoop()
             self.loop.run()
 
 
 class Py3status:
-    # available configuration options
-    allow_urgent = False
+    """
+    """
     button_allow = 1
     button_block = None
-    button_reject = 3
-    format = u'[UsbGuard: [{device_name}]]'
+    button_reject = 2
+    format = u'[USBGuard: [{device_name}]]'
 
     def post_config_hook(self):
         self.device = {
@@ -120,24 +117,26 @@ class Py3status:
             'device_id': None,
             'device_hash': None
         }
+        self.dbus_interface = 'org.usbguard'
+        self.dbus_devices = '/org/usbguard/Devices'
         self.error = None
         self.dbus = SystemBus()
         try:
-            self.proxy = self.dbus.get(DBUS_INTERFACE)
+            self.proxy = self.dbus.get(self.dbus_interface)
         except:
-            self.parent.error = Exception("usbguard-dbus service not running")
+            self.parent.error = Exception(STRING_USBGUARD_DBUS)
 
         self.targets = {'allow': 0, 'block': 1, 'reject': 2}
+        self.permanant = False
         self.killed = threading.Event()
         UsbguardListener(self).start()
 
-    def _usbguard_dbus_cmd(self, action):
-        return self.proxy.applyDevicePolicy(self.device['device_id'],
-                                            self.targets[action], 0)
+    def _usbguard_cmd(self, action):
+        return self.proxy.applyDevicePolicy(
+            self.device['device_id'], self.targets[action], self.permanant)
 
     def usbguard(self):
-        """
-        """
+        urgent = True
         if self.error:
             self.py3.error(str(self.error), self.py3.CACHE_FOREVER)
 
@@ -145,24 +144,22 @@ class Py3status:
             'cached_until': self.py3.CACHE_FOREVER,
             'full_text': self.py3.safe_format(self.format, self.device)
         }
-        if self.allow_urgent:
-            response['urgent'] = True
+        if not urgent:
+            response['urgent'] = False
+
         return response
 
     def on_click(self, event):
-        """
-        """
         button = event['button']
         action = None
-        if self.button_allow and button == self.button_allow:
+        if button == self.button_allow:
             action = 'allow'
-        elif self.button_reject and button == self.button_reject:
+        elif button == self.button_reject:
             action = 'reject'
-        elif self.button_block and button == self.button_block:
+        elif button == self.button_block:
             action = 'block'
-
         if action:
-            self._usbguard_dbus_cmd(action)
+            self._usbguard_cmd(action)
             sleep(0.1)
             self.py3.update()
 
