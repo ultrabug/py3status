cd28f9831fee8e919751bd6bad706c7ef59fdf2f
Ultrabug <ultrabug@gentoo.org>
2018-08-10 00:22:57 +0200

    black migration: setup

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ from setuptools.command.install import install
 from setuptools.command.easy_install import _to_ascii, ScriptWriter
 
 module_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'py3status')
+    os.path.dirname(os.path.realpath(__file__)), "py3status"
+)
 sys.path.insert(0, module_path)
 from version import version  # noqa
+
 sys.path.remove(module_path)
 
 
@@ -38,7 +40,7 @@ def install_py3_cmd(installer):
     py_cmd = ScriptWriter.get_header()
     script_text = PY3_CMD_SCRIPT_TEXT.format(py_cmd)
     try:
-        installer.write_script('py3-cmd', _to_ascii(script_text), 'b')
+        installer.write_script("py3-cmd", _to_ascii(script_text), "b")
     except AttributeError:
         # building wheel etc
         pass
@@ -69,41 +71,39 @@ def read(fname):
 
 
 setup(
-    name='py3status',
+    name="py3status",
     version=version,
-    author='Ultrabug',
-    author_email='ultrabug@ultrabug.net',
-    description='py3status: an extensible i3status wrapper written in python',
-    long_description=read('README.rst'),
-    extras_require={'gevent': ['gevent >= 1.1']},
-    url='https://github.com/ultrabug/py3status',
-    download_url='https://github.com/ultrabug/py3status/tags',
-    license='BSD',
-    platforms='any',
+    author="Ultrabug",
+    author_email="ultrabug@ultrabug.net",
+    description="py3status: an extensible i3status wrapper written in python",
+    long_description=read("README.rst"),
+    extras_require={"gevent": ["gevent >= 1.1"]},
+    url="https://github.com/ultrabug/py3status",
+    download_url="https://github.com/ultrabug/py3status/tags",
+    license="BSD",
+    platforms="any",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
+    cmdclass={"develop": PostDevelopCommand, "install": PostInstallCommand},
     entry_points={
-        'console_scripts': [
-            'py3status = py3status:main',
-            'py3-cmd = py3status.command:send_command',
+        "console_scripts": [
+            "py3status = py3status:main",
+            "py3-cmd = py3status.command:send_command",
         ]
     },
     classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ], )
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
