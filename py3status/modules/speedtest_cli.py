e0560d55a1926b436c681c02475e48369d32abc6
Cyril Levis <levis.cyril@gmail.com>
2018-08-18 17:02:12 +0200

    documentation

--- py3status/modules/speedtest_cli.py
+++ py3status/modules/speedtest_cli.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Do a bandwidth test with speedtest-cli.
+Do a bandwidth test with speedtest-cli. 
 
 Configuration parameters:
     cache_timeout: refresh interval for this module (default -1)
@@ -11,13 +11,29 @@ Configuration parameters:
         (default 20)
     thresholds: specify color thresholds to use
         (default [(0, 'bad'), (11, 'good')])
+    unit_bitrate: unit for download/upload rate (default 'MB/s')
+    unit_size: unit for bytes_received/bytes_sent (default 'MB')
 
 Format placeholders:
-    {download} download rate from speedtest server (in MB/s)
+    {download} download rate from speedtest server, human readable
+    {download_raw} download rate from speedtest server, for format comparation
+    {download_unit} unit used as , eg 'MB/s'
     {ping} ping time in ms to speedtest server
-    {upload} upload rate to speedtest server (in MB/s)
-    {bytes_sent} bytes sent during test (in MB)
-    {bytes_received} bytes received during test (in MB)
+    {upload} upload rate to speedtest server, human readable
+    {upload_raw} upload rate to speedtest server, for format comparation
+    {upload_unit} unit used for upload, eg 'MB/s'
+    {bytes_sent} bytes sent during test, human readable
+    {bytes_sent_unit} unit used for bytes_sent, eg 'MB'
+    {bytes_sent_raw} bytes sent during test, for format comparation
+    {bytes_received} bytes received during test, human readable
+    {bytes_received_raw} bytes received during test, for format comparation
+    {bytes_received_unit} unit used for bytes_received, eg 'MB'
+    {timestamp} timestamp of the run
+
+The module will be triggered on clic only. Not at start.
+
+Note that all placeholders have a version prefixed by `last_`, eg `last_download`.
+This can be usefull to compare two runs.
 
 Requires:
     speedtest-cli: Command line interface for testing internet bandwidth using speedtest.net
@@ -37,7 +53,14 @@ speedtest_cli {
             (0, 'bad'), (1024, 'degraded'), (1024 * 1024, 'good')
         ],
     }
-    format = '[\?color=ping {ping} ms] [\?color=upload ↑ {upload}] [\?color=download ↓ {download}]'
+    format = '[\?color=ping {ping} ms] [\?color=upload ↑ {upload} {upload_unit}]'
+    format += ' [\?color=download ↓ {download} {download_unit}]'
+}
+
+
+# colored based on comparation between two last runs
+speedtest_cli {
+    format = ' [[\?if=download_raw<last_download_raw&color=degraded ↓]|[\?color=ok ↑]]'
 }
 
 @author Cyril Levis (@cyrinux)
@@ -51,13 +74,12 @@ from json import loads
 
 STRING_NOT_INSTALLED = 'not installed'
 
-
 class Py3status:
     """
     """
     # available configuration parameters
     cache_timeout = -1
-    format = u'\u25cf [{ping} ms] [↑ {upload}] [↓ {download}]'
+    format = u'\u25cf [{ping} ms] [↑ {upload} {upload_unit}] [↓ {download} {download_unit}]'
     si_units = False
     sleep_timeout = 20
     thresholds = {
@@ -71,6 +93,8 @@ class Py3status:
             (0, 'bad'), (1024, 'degraded'), (1024 * 1024, 'good')
         ],
     }
+    unit_bitrate = 'MB/s'
+    unit_size = 'MB'
 
     def post_config_hook(self):
         if not self.py3.check_commands('speedtest-cli'):
@@ -79,6 +103,7 @@ class Py3status:
         self.placeholders = list(
             set(self.py3.get_placeholders_list(self.format))
         )
+        self.run = False
         self.speedtest_command = 'speedtest-cli --json --secure'
 
     def _is_running(self):
@@ -111,28 +136,37 @@ class Py3status:
         speedtest_data = {}
         cached_until = self.sleep_timeout
 
-        if not self._is_running():
+        if not self._is_running() and self.run:
             last_speedtest_data = self._get_last_speedtest_data()
             speedtest_data = self._get_speedtest_data()
             cached_until = self.cache_timeout
+
             if speedtest_data:
                 self.py3.storage_set('speedtest_data', speedtest_data)
                 if last_speedtest_data:
                     speedtest_data.update(last_speedtest_data)
 
+                # raw version and units convertion
+                for x in ['download', 'upload']:
+                    speedtest_data[x + '_raw'] = speedtest_data[x]
+                    speedtest_data[x], speedtest_data[x + '_unit'] = self.py3.format_units(
+                        speedtest_data[x], unit=self.unit_bitrate, si=self.si_units
+                    )
+
+                for x in ['bytes_received', 'bytes_sent']:
+                    speedtest_data[x + '_raw'] = speedtest_data[x]
+                    speedtest_data[x], speedtest_data[x + '_unit'] = self.py3.format_units(
+                        speedtest_data[x], unit=self.unit_size, si=self.si_units
+                    )
+                    
                 # thresholds
                 for x in self.thresholds:
                     if x in speedtest_data:
                         self.py3.threshold_get_color(speedtest_data[x], x)
-
-                # units
-                for x in ['download', 'upload']:
-                    rate, unit = self.py3.format_units(speedtest_data[x], unit='MB/s', si=self.si_units)
-                    speedtest_data[x] = str(rate) + ' ' + unit
-
-                for x in ['bytes_received', 'bytes_sent']:
-                    size, unit = self.py3.format_units(speedtest_data[x], unit='MB', si=self.si_units)
-                    speedtest_data[x] = str(size) + ' ' + unit
+        else:
+            # little hack for prevent running speedtest
+            # at start, allow only trigger on_click / refresh
+            self.run = True
 
         return {
             'cached_until': self.py3.time_in(cached_until),
@@ -141,6 +175,7 @@ class Py3status:
 
 
     def on_click(self, event):
+        # trigger refresh on any click
         pass
 
 if __name__ == "__main__":
