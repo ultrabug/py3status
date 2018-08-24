6f086c4b77900786da47b7bf849678dbd0e12cf9
Cyril Levis <levis.cyril@gmail.com>
2018-08-24 19:31:06 +0200

    round 2

--- py3status/modules/speedtest.py
+++ py3status/modules/speedtest.py
@@ -6,7 +6,7 @@ Configuration parameters:
     button_refresh: button to run the check (default 2)
     button_share: button to open the result link (default None)
     format: display format for this module, {download} and/or {upload} required
-        (default '● [{ping} ms] [↑ {upload} {upload_unit}] [↓ {download} {download_unit}]')
+        (default 'Speedtest [Up {upload} {upload_unit}] [Down {download} {download_unit}]')
     server_id: speedtest server to use, `speedtest-cli --list` to get id (default None)
     si_units: use SI units (default False)
     sleep_timeout: when speedtest-cli is allready running, this interval will be used
@@ -25,42 +25,42 @@ Configuration parameters:
     unit_size: unit for bytes_received/bytes_sent (default 'MB')
 
 Format placeholders:
-    {bytes_sent} bytes sent during test, human readable
-    {bytes_sent_unit} unit used for bytes_sent, eg 'MB'
-    {bytes_sent_raw} bytes sent during test, for format comparation
-    {bytes_received} bytes received during test, human readable
-    {bytes_received_raw} bytes received during test, for format comparation
-    {bytes_received_unit} unit used for bytes_received, eg 'MB'
-    {client_country} client country code, eg 'FR'
-    {client_ip} client ip, eg '78.194.13.7'
-    {client_isp} client isp, eg 'Free SAS'
-    {client_ispdlavg} client isp download average, eg '0'
-    {client_isprating} client isp rating, eg 3.7
-    {client_ispulavg} client isp upload average, eg '0'
-    {client_lat} client latitude, eg '48.8534'
-    {client_loggedin} client logged in, eg '0'
-    {client_lon} client longitude, eg '2.3487999999999998'
-    {client_rating} client rating, eg '0'
-    {download} download rate from speedtest server, human readable
-    {download_raw} download rate from speedtest server, for format comparation
-    {download_unit} unit used as , eg 'MB/s'
-    {ping} ping time in ms to speedtest server
-    {quality} quality of the connection, eg ok, bad, faster, slower
-    {timestamp} timestamp of the run
-    {server_cc} server country code, eg 'FR'
-    {server_country} server country, eg 'France'
-    {server_d} server distance, eg '2.316599376968091'
-    {server_host} server host, eg 'speedtest.telecom-paristech.fr:8080'
-    {server_id} server id, eg '11977'
-    {server_lat} server latitude, eg '48.8742'
-    {server_latency} server latency, eg 8.265
-    {server_lon} server longitude, eg 2.3470
-    {server_name} server name, eg 'Paris'
-    {server_sponsor} server sponsor, eg 'Télécom ParisTech'
-    {server_url} server url, eg 'http://speedtest.telecom-paristech.fr/upload.php'
-    {upload} upload rate to speedtest server, human readable
-    {upload_raw} upload rate to speedtest server, for format comparation
-    {upload_unit} unit used for upload, eg 'MB/s'
+    {bytes_sent}            bytes sent during test, human readable
+    {bytes_sent_unit}       unit used for bytes_sent, eg 'MB'
+    {bytes_sent_raw}        bytes sent during test, for format comparation
+    {bytes_received}        bytes received during test, human readable
+    {bytes_received_raw}    bytes received during test, for format comparation, eg
+    {bytes_received_unit}   unit used for bytes_received, eg 'MB'
+    {client_country}        client country code, eg 'FR'
+    {client_ip}             client ip, eg '78.194.13.7'
+    {client_isp}            client isp, eg 'Free SAS'
+    {client_ispdlavg}       client isp download average, eg '0'
+    {client_isprating}      client isp rating, eg 3.7
+    {client_ispulavg}       client isp upload average, eg '0'
+    {client_lat}            client latitude, eg '48.8534'
+    {client_loggedin}       client logged in, eg '0'
+    {client_lon}            client longitude, eg '2.3487999999999998'
+    {client_rating}         client rating, eg '0'
+    {download}              download rate from speedtest server, human readable
+    {download_raw}          download rate from speedtest server, for format comparation
+    {download_unit}         unit used as , eg 'MB/s'
+    {ping}                  ping time in ms to speedtest server
+    {quality}               quality of the connection, eg ok, bad, faster, slower
+    {timestamp}             timestamp of the run, eg 'TODO'
+    {server_cc}             server country code, eg 'FR'
+    {server_country}        server country, eg 'France'
+    {server_d}              server distance, eg '2.316599376968091'
+    {server_host}           server host, eg 'speedtest.telecom-paristech.fr:8080'
+    {server_id}             server id, eg '11977'
+    {server_lat}            server latitude, eg '48.8742'
+    {server_latency}        server latency, eg 8.265
+    {server_lon}            server longitude, eg 2.3470
+    {server_name}           server name, eg 'Paris'
+    {server_sponsor}        server sponsor, eg 'Télécom ParisTech'
+    {server_url}            server url, eg 'http://speedtest.telecom-paristech.fr/upload.php'
+    {upload}                upload rate to speedtest server, human readable
+    {upload_raw}            upload rate to speedtest server, for format comparation
+    {upload_unit}           unit used for upload, eg 'MB/s'
 
 Color thresholds:
     format:
@@ -73,6 +73,8 @@ This can be usefull to compare two runs.
 
 Requires:
     speedtest-cli: Command line interface for testing internet bandwidth using speedtest.net
+
+@author Cyril Levis (@cyrinux)
 
 Examples:
 ```
@@ -90,13 +92,14 @@ speedtest {
         ],
         "upload": [(0, "bad"), (1024, "degraded"), (1024 * 1024, "good")],
     }
-    format = '[\?color=ping {ping} ms] [\?color=upload ↑ {upload} {upload_unit}]'
-    format += ' [\?color=download ↓ {download} {download_unit}]'
+    format = '[\?color=ping {ping} ms] [\?color=upload Up {upload} {upload_unit}]'
+    format += ' [\?color=download Down {download} {download_unit}]'
 }
 
 # colored based on comparation between two last runs
 speedtest {
-    format = ' [[\?if=download_raw<previous_download_raw&color=degraded ↓]|[\?color=ok ↑]]'
+    format = '[[\?if=download_raw<previous_download_raw&color=degraded Faster]'
+    format += '|[\?color=ok Slower]]'
 }
 
 # compare only download
@@ -105,14 +108,12 @@ speedtest {
     format += '[\?if=quality=faster&color=download_raw ↑ {download} {download_unit}]'
     format += '[\?if=quality=ok&color=download_raw -> {download} {download_unit}]'
     format += '[\?if=quality=bad&color=download_raw x {download} {download_unit}]]'
-    format += '|'
+    format += '|speedtest'
 }
 ```
 
-@author Cyril Levis (@cyrinux)
-
 SAMPLE OUTPUT
-{'full_text': u'\u25cf 208.18 ms \u2191 0.247 MB/s \u2193 0.453 MB/s'}
+{'full_text': u'Speedtest Down 0.247 MB/s Up 0.453 MB/s'}
 """
 
 from json import loads
@@ -130,7 +131,7 @@ class Py3status:
     button_refresh = 2
     button_share = None
     format = (
-        u"\u25cf [{ping} ms] [↑ {upload} {upload_unit}] [↓ {download} {download_unit}]"
+        u"Speedtest [Up {upload} {upload_unit}] [Down {download} {download_unit}]"
     )
     server_id = None
     si_units = False
@@ -159,33 +160,29 @@ class Py3status:
 
         self.placeholders = self.py3.get_placeholders_list(self.format)
 
+        self.command = ["speedtest-cli --json --secure"]
+
         # if download* or upload* missing, run complete test
-        no_upload = ""
-        no_download = ""
         if any(
             key.startswith(x)
             for x in ["download", "upload"]
             for key in self.placeholders
         ):
             if not any(key.startswith("upload") for key in self.placeholders):
-                no_upload = "--no-upload"
+                self.command += ["--no-upload"]
 
             if not any(key.startswith("download") for key in self.placeholders):
-                no_download = " --no-download"
+                self.command += ["--no-download"]
 
-        server = ""  # if not specified, nearest server is use
         if self.server_id and int(self.server_id):
-            server = "--server {}".format(self.server_id)
+            self.command += ["--server {}".format(self.server_id)]
 
-        share = ""
         if self.button_share and all(
             x in self.placeholders for x in ["download", "upload"]
         ):
-            share = "--share"
+            self.command += ["--share"]
 
-        self.command = "speedtest-cli --json --secure --timeout {} {} {} {} {}".format(
-            self.timeout, no_upload, no_download, server, share
-        )
+        self.command += ["--timeout {}".format(self.timeout)]
 
     def _is_running(self):
         try:
@@ -195,7 +192,8 @@ class Py3status:
             return False
 
     def _get_speedtest_data(self):
-        return loads(self.py3.command_output(self.command)) or None
+        command = ' '.join(self.command)
+        return loads(self.py3.command_output(command)) or None
 
     def speedtest(self):
         speedtest_data = {}
@@ -253,8 +251,7 @@ class Py3status:
             self.py3.storage_set("speedtest_data", current_data)
 
             # get speedtest result url
-            if current_data and "share" in current_data:
-                self.url = current_data["share"]
+            self.url = current_data.get("share")
 
             # create placeholders
             speedtest_data.update(current_data)
@@ -286,7 +283,7 @@ class Py3status:
         button = event["button"]
         if button == self.button_share and self.url:
             self.py3.command_run("xdg-open %s" % self.url)
-        elif button != self.button_refresh:
+        if button != self.button_refresh:
             self.py3.prevent_refresh()
 
 
