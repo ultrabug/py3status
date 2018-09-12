fe0ab7d86a55bf74decd14aba1c37557d56ca40d
Cyril Levis <levis.cyril@gmail.com>
2018-09-12 12:02:29 +0200

    v1

--- py3status/modules/velib_metropole.py
+++ py3status/modules/velib_metropole.py
@@ -1,29 +1,45 @@
 # -*- coding: utf-8 -*-
 """
 Display velib available to a station.
-# https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json
-# curl 'https://www.velib-metropole.fr/webapi/map/details?gpsTopLatitude=48.85170875805659&gpsTopLongitude=2.3991161073518583&gpsBotLatitude=48.85146608086396&gpsBotLongitude=2.397838705306725&zoomLevel=20' | jq .
+
+Configuration parameters:
+    button_next: Display next station (default 3)
+    button_previous: Display previous station (default 1)
+    button_refresh: refresh stations (default 2)
+    cache_timeout: The time between API polling in seconds
+        It is recommended to keep this at a higher value to avoid rate
+        limiting with the API's. (default 60)
+    format: How to display the velib data.
+        (default 'Velib Metropole: {index}/{stations} {format_station}')
+    format_station: How to display the velib station data.
+        (default '{station_name}: [\\?color=station_state_code {station_state}]')
+    station_codes: List of velib station to monitor.
+        You can get stations id on map here:
+        https://www.velib-metropole.fr/map  (default [20043, 11014, 20012, 20014, 10042])
+    thresholds: Configure colors of format station.
+        (default {'station_state_code': [(0, 'good'), (1, 'bad')]})
 """
 from datetime import datetime
 from re import sub
+
+STRING_MISSING_STATIONS = "No velib stations set"
 
 
 class Py3status:
     """
     """
+
     # available configuration parameters
+    button_next = 3
+    button_previous = 1
+    button_refresh = 2
     cache_timeout = 60
-    # format = '[\?color=aqi {city_name}: {aqi} {category}]'
-    format = u"{station_name}: {station_state} free: {nb_bike}/{nb_dock}"
-    gps_top_latitude = "48.8517"
-    gps_top_longitude = "2.399"
-    # gps_top_latitude = "48.87429034098611"
-    # gps_top_longitude = "2.3535048444482243"
-    gps_bot_latitude = str(float(gps_top_latitude) - 0.0003)
-    gps_bot_longitude = str(float(gps_top_longitude) - 0.002)
-    # gps_bot_latitude = "48.8514"
-    # gps_bot_longitude = "2.397"
-    station_code = "20043"
+    format = u"Velib Metropole: {index}/{stations} {format_station}"
+    format_station = u"{station_name}: [\?color=station_state_code {station_state}]"
+    format_station += "[\?soft  ][\?color=greenyellow {nb_bike}/{nb_free_e_dock}]"
+    format_station += "[\?soft  ][\?color=deepskyblue {nb_ebike}/{nb_free_e_dock}]"
+    station_codes = [20043, 11014, 20012, 20014, 10042]
+    thresholds = {"station_state_code": [(0, "good"), (1, "bad")]}
 
     class Meta:
         update_config = {
@@ -42,27 +58,41 @@ class Py3status:
                         "nb_free_e_dock": ":.0f",
                         "station_gps_latitude": ":.3f",
                         "station_gps_longitude": ".3f",
+                        "station_due_date": ".0f",
                     },
-                    "format_strings": ["format"],
+                    "format_strings": ["format_station"],
                 }
             ]
         }
 
     def post_config_hook(self):
-        self.request_timeout = 30
-        self.url = (
-            "https://www.velib-metropole.fr/webapi/map/details?gpsTopLatitude=%s&gpsTopLongitude=%s&gpsBotLatitude=%s&gpsBotLongitude=%s&zoomLevel=%s"
-            % (
-                self.gps_top_latitude,
-                self.gps_top_longitude,
-                self.gps_bot_latitude,
-                self.gps_bot_longitude,
-                20,
-            )
-        )
-        self.py3.log(self.url)
-        self.placeholders = self.py3.get_placeholders_list(self.format)
-        self.velib_data = {}
+        if not self.station_codes:
+            raise Exception("%s" % (STRING_MISSING_STATIONS))
+
+        self.request_timeout = 10
+
+        # take the whole map for first run
+        # zone is shrinked later
+        self.gps_top_right_latitude = 49.0
+        self.gps_top_right_longitude = 2.5
+        self.gps_bot_left_latitude = 48.6
+        self.gps_bot_left_longitude = 2.2
+        self.gps_zoom_level = 15
+
+        self.station_states = {"Operative": 0, "Close": 1}
+
+        json = self._get_velib_data()
+        if json:
+            self.number_of_stations, self.stations = self._manipulate(json)
+            self._get_usefull_area(self.stations)
+
+        # get placeholders
+        self.placeholders = []
+        for x in ["format", "format_station"]:
+            self.placeholders.append(self.py3.get_placeholders_list(x))
+
+        self.refresh = True
+        self.station_index = 1
 
     def _camel_to_snake_case(self, data):
         if not isinstance(data, (int, float)):
@@ -81,40 +111,133 @@ class Py3status:
                 pass
         return value
 
+    def _get_usefull_area(self, data):
+        """
+        reduce the zone to reduce the size of fetched data on refresh
+        """
+
+        latitudes = []
+        longitudes = []
+        for x in data:
+            latitudes.append(float(data[x]["station_gps_latitude"]))
+            longitudes.append(float(data[x]["station_gps_longitude"]))
+        self.gps_top_right_latitude = max(latitudes)
+        self.gps_top_right_longitude = max(longitudes)
+        self.gps_bot_left_latitude = min(latitudes)
+        self.gps_bot_left_longitude = min(longitudes)
+
     def _get_velib_data(self):
+        url = "".join(
+            [
+                "https://www.velib-metropole.fr/webapi/map/details?",
+                "gpsTopLatitude=%s",
+                "&gpsTopLongitude=%s",
+                "&gpsBotLatitude=%s",
+                "&gpsBotLongitude=%s",
+                "&zoomLevel=%s",
+            ]
+        )
+        url = url % (
+            self.gps_top_right_latitude,
+            self.gps_top_right_longitude,
+            self.gps_bot_left_latitude,
+            self.gps_bot_left_longitude,
+            self.gps_zoom_level,
+        )
+
         try:
-            return self.py3.request(self.url, timeout=self.request_timeout).json()
+            return self.py3.request(url, timeout=self.request_timeout).json()
         except self.py3.RequestException:
             return None
 
     def _manipulate(self, data):
-        # flat data
-        # convert key from camel to snake case
-        new_data = {}
-        for k, v in self.py3.flatten_dict(data, delimiter="_").items():
-            new_data[self._camel_to_snake_case("".join(k.replace("0_", "", 1)))] = v
-        return new_data
+        index = 1
+        stations = {}
+
+        for code in self.station_codes:
+            new_station = {}
+
+            # search station
+            station = next(
+                (item for item in data if int(item["station"]["code"]) == int(code)),
+                None,
+            )
+
+            # flat, camel to snake and cast...
+            for k, v in self.py3.flatten_dict(station, delimiter="_").items():
+                new_station[self._camel_to_snake_case(k)] = self._cast_number(v)
+
+            # station_due_date_s: station due date in dateime iso format
+            # station_state_code: station code for thresholds
+            station_due_date = datetime.fromtimestamp(
+                int(new_station["station_due_date"])
+            )
+            new_station.update(
+                {
+                    "station_due_date_s": station_due_date.isoformat(),
+                    "station_state_code": int(
+                        self.station_states[new_station["station_state"]]
+                    ),
+                }
+            )
+
+            stations[index] = new_station
+            index += 1
+
+        return len(stations), stations
 
     def velib_metropole(self):
-        self.velib_data = self._get_velib_data()
-        # for k,v in self.velib_data:
-        #     if self.velib_data[k] == "stationCode":
-        #         if v == self.station_code:
-        #             self.py3.log('pinnnng: k + %s, v: %s'.format(k, v))
+        if self.refresh is True:
+            # get number of stations and station list
+            json = self._get_velib_data()
+            if json:
+                self.number_of_stations, self.stations = self._manipulate(json)
 
-        self.velib_data = self._manipulate(self.velib_data)
-        if self.velib_data:
-            self.velib_data = self._manipulate(self.velib_data)
-            self.velib_data.update(
-                {k: self._cast_number(self.velib_data[k]) for k in self.placeholders}
-            )
-            
-        self.py3.log(self.velib_data)
+        # reset refresh
+        self.refresh = True
+
+        if not self.stations:
+            full_text = None
+        else:
+            # reset station_index counter
+            if self.station_index == 0:
+                self.station_index = 1
+
+            # thresholds
+            for x in self.thresholds:
+                if x in self.stations[self.station_index]:
+                    self.py3.threshold_get_color(
+                        self.stations[self.station_index][x], x
+                    )
+
+            # forge data output
+            velib_data = {
+                "stations": self.number_of_stations,
+                "format_station": self.py3.safe_format(
+                    self.format_station, self.stations[self.station_index]
+                ),
+                "index": self.station_index,
+            }
+            full_text = self.py3.safe_format(self.format, velib_data)
 
         return {
             "cached_until": self.py3.time_in(self.cache_timeout),
-            "full_text": self.py3.safe_format(self.format, self.velib_data),
+            "full_text": full_text,
         }
+
+    def on_click(self, event):
+        button = event["button"]
+        if self.stations:
+            if button == self.button_next:
+                self.refresh = False
+                self.station_index += 1
+                self.station_index %= self.number_of_stations + 1
+            elif button == self.button_previous:
+                self.refresh = False
+                self.station_index -= 1
+                self.station_index %= self.number_of_stations + 1
+        elif button == self.button_refresh:
+            self.refresh = True
 
 
 if __name__ == "__main__":
@@ -124,24 +247,3 @@ if __name__ == "__main__":
     from py3status.module_test import module_test
 
     module_test(Py3status)
-    #    {'credit_card': 'yes',
-    # 'density_level': 1,
-    # 'kiosk_state': 'yes',
-    # 'max_bike_overflow': 27,
-    # 'nb_bike': 12,
-    # 'nb_bike_overflow': 0,
-    # 'nb_dock': 27,
-    # 'nb_e_bike_overflow': 0,
-    # 'nb_e_dock': 0,
-    # 'nb_ebike': 1,
-    # 'nb_free_dock': 14,
-    # 'nb_free_e_dock': 0,
-    # 'overflow': 'no',
-    # 'overflow_activation': 'no',
-    # 'station_code': '20043',
-    # 'station_due_date': 1529013600,
-    # 'station_gps_latitude': 48.85160266522164,
-    # 'station_gps_longitude': 2.398403047037931,
-    # 'station_name': 'Charonne - Avron',
-    # 'station_state': 'Close',
-    # 'station_type': 'no'}
