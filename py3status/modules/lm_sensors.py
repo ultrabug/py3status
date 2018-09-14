d3367c2592442b11e4ec55f0a76218783bdba1f9
lasers <lasers@users.noreply.github.com>
2018-09-14 03:03:49 -0500

    lm_sensors: install lm_sensors-git

--- py3status/modules/lm_sensors.py
+++ py3status/modules/lm_sensors.py
@@ -178,7 +178,7 @@ from fnmatch import fnmatch
 from json import loads as json_loads
 
 STRING_NOT_INSTALLED = 'not installed'
-STRING_OLD_VERSION = 'install version 3.4.0+'
+STRING_GIT_VERSION = 'install 3.4.0+git with JSON support'
 SENSOR_NAMES = [
     'alarm', 'beep', 'crit', 'crit_alarm', 'crit_hyst', 'emergency',
     'emergency_hyst', 'input', 'max', 'min', 'offset' 'type',
@@ -208,7 +208,7 @@ class Py3status:
         try:
             lm_sensors_data = self._get_lm_sensors_data()
         except self.py3.CommandError:
-            raise Exception(STRING_OLD_VERSION)
+            raise Exception(STRING_GIT_VERSION)
 
         self.first_run = True
         if not self.py3.format_contains(self.format_chip, 'adapter'):
