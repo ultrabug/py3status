ultrabug/py3status

commit f4a32878ccb834d89cbcbca8c4a9d3c9980f214f

imap: add depreciation stuffs

py3status/modules/imap.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
-# FIXME new_mail_color param
 """
 Display number of unread messages from IMAP account.
 
 Configuration parameters:
-    cache_timeout: how often to run this check (default 60)
+    cache_timeout: refresh interval for this module (default 60)
     criterion: status of emails to check for (default 'UNSEEN')
-    format: format to display (default 'Mail: {unseen}')
-    hide_if_zero: don't show on bar if True (default False)
-    imap_server: IMAP server to connect to (default '<IMAP_SERVER>')
+    format: display format for this module (default 'Mail: {unseen}')
+    hide_if_zero: hide this module when no new mail (default False)
     mailbox: name of the mailbox to check (default 'INBOX')
-    new_mail_color: what color to output on new mail (default '')
-    password: login password (default '<PASSWORD>')
-    port: IMAP server port (default '993')
-    security: what authentication method is used: 'ssl' or 'starttls'
+    password: login password (default None)
+    port: number to use (default '993')
+    security: login authentication method: 'ssl' or 'starttls'
         (startssl needs python 3.2 or later) (default 'ssl')
-    user: login user (default '<USERNAME>')
+    server: server to connect (default None)
+    user: login user (default None)
 
 Format placeholders:
     {unseen} number of unread emails
 
+Color options:
+    color_good: use color when new mail arrives
+
 @author obb
 """
-
 import imaplib
 from ssl import create_default_context
+STRING_UNAVAILABLE = 'N/A'
 
 
 class Py3status:
@@ -35,13 +36,28 @@ class Py3status:
     criterion = 'UNSEEN'
     format = 'Mail: {unseen}'
     hide_if_zero = False
-    imap_server = '<IMAP_SERVER>'
     mailbox = 'INBOX'
-    new_mail_color = ''
-    password = '<PASSWORD>'
+    password = None
     port = '993'
     security = 'ssl'
-    user = '<USERNAME>'
+    server = None
+    user = None
+
+    class Meta:
+        deprecated = {
+            'rename': [
+                {
+                    'param': 'new_mail_color',
+                    'new': 'color_new_mail',
+                    'msg': 'obsolete parameter use `color_good`',
+                },
+                {
+                    'param': 'imap_server',
+                    'new': 'server',
+                    'msg': 'obsolete parameter use `server`',
+                },
+            ],
+        }
 
     def post_config_hook(self):
         if self.security not in ["ssl", "starttls"]:
@@ -52,32 +68,26 @@ class Py3status:
 
         response = {'cached_until': self.py3.time_in(self.cache_timeout)}
 
-        if not self.new_mail_color:
-            self.new_mail_color = self.py3.COLOR_GOOD
-
-        if mail_count == 'N/A':
-            response['full_text'] = mail_count
-        elif mail_count != 0:
-            response['color'] = self.new_mail_color
+        if mail_count is None:
+            response['color'] = self.py3.COLOR_BAD,
             response['full_text'] = self.py3.safe_format(
-                self.format, {'unseen': mail_count}
-            )
+                self.format, {'unseen': STRING_UNAVAILABLE})
+        elif mail_count > 0:
+            response['color'] = self.py3.COLOR_GOOD
+
+        if mail_count == 0 and self.hide_if_zero:
+            response['full_text'] = ''
         else:
-            if self.hide_if_zero:
-                response['full_text'] = ''
-            else:
-                response['full_text'] = self.py3.safe_format(
-                    self.format, {'unseen': mail_count}
-                )
+            response['full_text'] = self.py3.safe_format(self.format, {'unseen': mail_count})
 
         return response
 
     def _connection_ssl(self):
-        connection = imaplib.IMAP4_SSL(self.imap_server, self.port)
+        connection = imaplib.IMAP4_SSL(self.server, self.port)
         return connection
 
     def _connection_starttls(self):
-        connection = imaplib.IMAP4(self.imap_server, self.port)
+        connection = imaplib.IMAP4(self.server, self.port)
         connection.starttls(create_default_context())
         return connection
 
@@ -101,9 +111,8 @@ class Py3status:
 
             connection.close()
             return mail_count
-
         except:
-            return 'N/A'
+            return None
 
 
 if __name__ == "__main__":

commit 9fb54ab695a5f33c838f4217477d147d8acb360e

module imap: fix output formatting issue
This bugfix is two-fold: for once, a bug in handling mail_count==None
was introduced in f4a3287, instead of showing 'N/A' 'None' was printed,
and secondly, when IDLE is used, we attempted to format the response
before data was available, resulting again in 'None' to be outputted.

py3status/modules/imap.py
@@ -38,6 +38,7 @@ from time import sleep
 from ssl import create_default_context
 from socket import error as socket_error
 STRING_UNAVAILABLE = 'N/A'
+NO_DATA_YET = -1
 
 
 class Py3status:
@@ -77,7 +78,7 @@ class Py3status:
 
     def post_config_hook(self):
         # class variables:
-        self.mail_count = None
+        self.mail_count = NO_DATA_YET
         self.connection = None
         self.mail_error = None  # cannot throw self.py3.error from thread
         self.command_tag = 0  # IMAPcommands are tagged, so responses can be matched up to requests
@@ -103,19 +104,19 @@ class Py3status:
             self.mail_error = None
 
         # II -- format response
+        response['full_text'] = self.py3.safe_format(self.format, {'unseen': self.mail_count})
+
         if self.mail_count is None:
             response['color'] = self.py3.COLOR_BAD,
             response['full_text'] = self.py3.safe_format(
                 self.format, {'unseen': STRING_UNAVAILABLE})
+        elif self.mail_count == NO_DATA_YET:
+            response['full_text'] = ''
+        elif self.mail_count == 0 and self.hide_if_zero:
+            response['full_text'] = ''
         elif self.mail_count > 0:
             response['color'] = self.py3.COLOR_NEW_MAIL or self.py3.COLOR_GOOD
-            if self.allow_urgent:
-                response['urgent'] = True
-
-        if self.mail_count == 0 and self.hide_if_zero:
-            response['full_text'] = ''
-        else:
-            response['full_text'] = self.py3.safe_format(self.format, {'unseen': self.mail_count})
+            response['urgent'] = self.allow_urgent
 
         return response
 