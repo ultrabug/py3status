a596db44d9d2d03148597b7b029cf021e54dc759
Jørn Sandvik Nilsson <joern.nilsson@gmail.com>
2019-03-27 13:11:38 +0100

    module for printing gpu device currently in use

---
+++ py3status/modules/nvidia_prime.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Display which graphics device is in use, nvidia or intel (for systems supporting prime-select)
+
+
+Configuration parameters:
+    cache_timeout: how often to update the bar (default 60)
+    format: (default 'GPU: {device}')
+
+Format placeholders:
+    {device} Name of device currently in use
+
+Requires:
+    nvidia-prime: nvidia's tool to switch graphics device (nvidia or intel)
+
+Example:
+```
+nvidia_prime {
+    format = "GPU: {device}"
+    cache_timeout: 600
+}
+
+```
+
+@author Jørn Sandvik Nilsson
+
+"""
+
+from py3status import exceptions
+
+class Py3status:
+    """
+    """
+
+    # available configuration parameters
+    cache_timeout = 60
+    format = "GPU: {device}"
+
+    def nvidia_prime(self):
+
+        device = "unknown"
+        try:
+            device = self.py3.command_output(["prime-selectt", "query"]).strip()
+        except exceptions.CommandError as e:
+            pass
+
+        return {
+            'full_text': self.py3.safe_format(self.format, dict(device=device)),
+            'cached_until': self.py3.time_in(self.cache_timeout) 
+        }
+
+if __name__ == "__main__":
+    """
+    Run module in test mode.
+    """
+    from py3status.module_test import module_test
+
+    module_test(Py3status)
