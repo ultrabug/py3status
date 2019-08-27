ultrabug/py3status

commit 66a4a2692636b9dfce362fa2ec2ee03d4f2f1f3c

wifi module: specify device by name or MAC address (#1834)

py3status/modules/wifi.py
@@ -8,7 +8,8 @@ Configuration parameters:
     blocks: a string, where each character represents quality level
         (default "_▁▂▃▄▅▆▇█")
     cache_timeout: Update interval in seconds (default 10)
-    device: specify device name to use, otherwise auto (default None)
+    device: specify name or MAC address of device to use, otherwise auto
+        (default None)
     down_color: Output color when disconnected, possible values:
         "good", "degraded", "bad" (default "bad")
     format: Display format for this module
@@ -106,11 +107,14 @@ class Py3status:
             data = self.py3.command_output([iw, "dev"])
         except self.py3.CommandError as ce:
             raise Exception(ce.error.strip())
+        last_device = None
         for line in data.splitlines()[1:]:
-            if "Interface" in line:
-                device = line.split()[-1]
-                if not self.device or device == self.device:
-                    self.device = device
+            if "Interface" in line or "addr" in line:
+                intf_or_addr = line.split()[-1]
+                if "Interface" in line:
+                    last_device = intf_or_addr
+                if not self.device or intf_or_addr.lower() == self.device.lower():
+                    self.device = last_device
                     break
         else:
             device = " `{}`".format(self.device) if self.device else ""

commit a42ed97e8c2dd2351523a59b280912c5c8bdaa70

wifi module: fix bug introduced in PR #1834

py3status/modules/wifi.py
@@ -109,7 +109,7 @@ class Py3status:
             raise Exception(ce.error.strip())
         last_device = None
         for line in data.splitlines()[1:]:
-            if "Interface" in line or "addr" in line:
+            if "Interface" in line or ("addr" in line and last_device is not None):
                 intf_or_addr = line.split()[-1]
                 if "Interface" in line:
                     last_device = intf_or_addr