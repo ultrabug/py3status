0f419586fb3d371623ac9f91c026eac84d402e53
lasers <lasers@users.noreply.github.com>
2019-10-25 09:55:23 -0500

    scratchpad: add swaymsg ipc

--- py3status/modules/scratchpad.py
+++ py3status/modules/scratchpad.py
@@ -3,7 +3,7 @@
 Display number of scratchpad windows and urgency hints.
 
 Configuration parameters:
-    cache_timeout: refresh interval for i3-msg (default 5)
+    cache_timeout: refresh interval for i3-msg or swaymsg (default 5)
     format: display format for this module
         (default "\u232b [\?color=scratchpad {scratchpad}]")
     thresholds: specify color thresholds to use
@@ -17,8 +17,7 @@ Color thresholds:
     xxx: print a color based on the value of `xxx` placeholder
 
 Optional:
-    i3ipc: an improved python library to control i3wm (async)
-    i3-msg: send messages to i3 window manager (counter)
+    i3ipc: an improved python library to control i3wm and sway
 
 Examples:
 ```
@@ -48,7 +47,7 @@ violet
 [{'full_text': '\u232b '}, {'full_text': u'5', 'color': '#ee82ee'}]
 
 urgent
-{'full_text': '\u232b URGENT 1', 'urgent': True}
+[{'full_text': '\u232b URGENT 1', 'urgent': True}]
 """
 
 STRING_ERROR = "invalid ipc `{}`"
@@ -62,19 +61,18 @@ class Ipc:
         self.parent = parent
         self.setup(parent)
 
-    def set_scratchpad_leaves(self):
-        pass
-
 
 class I3ipc(Ipc):
     """
-    i3ipc - an improved python library to control i3wm
+    i3ipc - an improved python library to control i3wm and sway
     """
 
     def setup(self, parent):
         from threading import Thread
 
         self.parent.cache_timeout = self.parent.py3.CACHE_FOREVER
+        self.nodes = []
+
         t = Thread(target=self.start)
         t.daemon = True
         t.start()
@@ -89,26 +87,30 @@ class I3ipc(Ipc):
         i3.main()
 
     def update(self, i3, event=None):
-        leaves = i3.get_tree().scratchpad().leaves()
-        self.parent.leaves = [window.urgent for window in leaves]
+        self.nodes = i3.get_tree().scratchpad().leaves()
         self.parent.py3.update()
 
+    def get_scratchpad_nodes(self):
+        return [window.urgent for window in self.nodes]
 
-class I3msg(Ipc):
+
+class Msg(Ipc):
     """
     i3-msg - send messages to i3 window manager
+    swaymsg - send messages to sway window manager
     """
 
     def setup(self, parent):
         from json import loads
 
         self.json_loads = loads
-        self.tree_command = [self.parent.py3.get_wm_msg(), "-t", "get_tree"]
+        wm_msg = {"i3msg": "i3-msg"}.get(parent.ipc, parent.ipc)
+        self.tree_command = [wm_msg, "-t", "get_tree"]
 
-    def set_scratchpad_leaves(self):
-        tree = self.parent.py3.command_output(self.tree_command)
-        leaves = self.find_scratchpad(self.json_loads(tree)).get("floating_nodes", [])
-        self.parent.leaves = [window["urgent"] for window in leaves]
+    def get_scratchpad_nodes(self):
+        tree = self.json_loads(self.parent.py3.command_output(self.tree_command))
+        nodes = self.find_scratchpad(tree).get("floating_nodes", [])
+        return [window["urgent"] for window in nodes]
 
     def find_scratchpad(self, tree):
         if tree.get("name") == "__i3_scratch":
@@ -130,35 +132,34 @@ class Py3status:
     thresholds = [(0, "darkgray"), (1, "violet")]
 
     def post_config_hook(self):
-        # ipc: specify i3ipc or i3-msg, otherwise auto (default None)
+        # ipc: specify i3ipc, i3-msg, or swaymsg, otherwise auto (default None)
         self.ipc = getattr(self, "ipc", "").replace("-", "")
         if self.ipc in ["", "i3ipc"]:
             try:
-                from i3ipc import Connection  # noqa f401, auto ipc
+                from i3ipc import Connection  # noqa f401
 
                 self.ipc = "i3ipc"
             except Exception:
-                if self.ipc in ["i3ipc"]:
-                    raise  # module i3ipc not found
-                self.ipc = "i3msg"
-        elif self.ipc not in ["i3msg"]:
+                if self.ipc:
+                    raise  # module not found
+
+        self.ipc = self.ipc or self.py3.get_wm_msg().replace("-", "")
+        if self.ipc in ["i3ipc"]:
+            self.backend = I3ipc(self)
+        elif self.ipc in ["i3msg", "swaymsg"]:
+            self.backend = Msg(self)
+        else:
             raise Exception(STRING_ERROR.format(self.ipc))
 
-        self.leaves = []
-        self.backend = globals()[self.ipc.capitalize()](self)
         self.thresholds_init = self.py3.get_color_names_list(self.format)
 
-    def _get_scratchpad_leaves(self):
-        self.backend.set_scratchpad_leaves()
-        return self.leaves
-
     def scratchpad(self):
-        scratchpad_leaves = self._get_scratchpad_leaves()
+        scratchpad_nodes = self.backend.get_scratchpad_nodes()
 
         scratchpad_data = {
             "ipc": self.ipc,
-            "scratchpad": len(scratchpad_leaves),
-            "urgent": sum(scratchpad_leaves),
+            "scratchpad": len(scratchpad_nodes),
+            "urgent": sum(scratchpad_nodes),
         }
 
         for x in self.thresholds_init:
@@ -180,6 +181,6 @@ if __name__ == "__main__":
     """
     from py3status.module_test import module_test
 
-    format = "\[{ipc}\] [\?color=scratchpad {scratchpad}]"
+    config = {"format": "\[{ipc}\] [\?color=scratchpad {scratchpad}]"}
 
-    module_test(Py3status, config={"format": format})
+    module_test(Py3status, config=config)
