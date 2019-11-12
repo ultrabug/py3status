437a576ea9a1c5940951b78d930620f32ba9cea5
lasers <lasers@users.noreply.github.com>
2019-11-11 22:46:12 -0600

    bluetooth: k

--- py3status/modules/bluetooth.py
+++ py3status/modules/bluetooth.py
@@ -4,53 +4,92 @@ Display bluetooth status.
 
 Configuration parameters:
     cache_timeout: refresh interval for this module (default 10)
-    format: display format for this module (default 'BT[: {format_device}]')
-    format_device: display format for bluetooth devices (default '{name}')
-    format_separator: show separator if more than one (default '\|')
+    format: display format for this module (default "{format_adapter}")
+    format_adapter: display format for adapters (default "{format_device}")
+    format_adapter_separator: show separator if more than one (default " ")
+    format_device: display format for devices
+        (default "\?if=connected&color=connected {alias}")
+    format_device_separator: show separator if more than one (default " ")
+    thresholds: specify color thresholds to use
+        (default [(False, "bad"), (True, "good")])
 
 Format placeholders:
-    {format_device} format for bluetooth devices
+    {format_adapter}      format for adapters
+    {adapter}             number of adapters, eg 1
+
+format_adapter placeholders:
+    {format_device}       format for devices
+    {device}              number of devices, eg 5
+    {address}             eg, 00:00:00:00:00:00
+    {addresstype}         eg, public
+    {alias}               eg, thinkpad
+    {class}               eg, 123456
+    {discoverable}        eg, False
+    {discoverabletimeout} eg, 0
+    {discovering}         eg, False
+    {modalias}            eg, usb:v1D68234ABCDEF5
+    {name}                eg, z420
+    {pairable}            eg, True
+    {pairabletimeout}     eg, 0
+    {path}                eg, /org/bluez/hci0
+    {powered}             eg, True
+    {uuids}               eg, []
 
 format_device placeholders:
-    {mac} bluetooth device address
-    {name} bluetooth device name
+    {adapter}          eg, /org/bluez/hci0
+    {address}          eg, 00:00:00:00:00:00
+    {addresstype}      eg, public
+    {alias}            eg, MSFT Mouse
+    {class}            eg, 1234
+    {connected}        eg, False
+    {icon}             eg, input-mouse
+    {legacypairing}    eg, False
+    {modalias}         eg, usb:v1D68234ABCDEF5
+    {name}             eg, Microsoft Bluetooth Notebook Mouse 5000
+    {paired}           eg, True
+    {servicesresolved} eg, False
+    {trusted}          eg, True
+    {uuids}            eg, []
 
-Color options:
-    color_bad: No connection
-    color_good: Active connection
+Color thresholds:
+    xxx: print a color based on the value of `xxx` placeholder
 
 Requires:
-    pydbus: pythonic dbus library
+    python-gobject: Python Bindings for GLib/GObject/GIO/GTK+
 
-@author jmdana <https://github.com/jmdana>
+Examples:
+```
+# always display devices
+bluetooth {
+    format_device = "\?color=connected {alias}"
+}
+
+# set an alias via blueman-manager or bluetoothctl
+# $ bluetoothctl
+# [bluetooth] # devices
+# [bluetooth] # connect 00:00:00:00:00:00
+# [bluetooth] # set-alias "MSFT Mouse"
+
+# display missing adapter (feat. request)
+bluetooth {
+    format = "\?if=adapter {format_adapter}|\?color=darkgray No Adapter"
+}
+
+# legacy default
+bluetooth {
+    format = "\?color=good BT: {format_adapter}|\?color=bad BT"
+    format_device_separator = "\|"
+}
+```
+
+@author jmdana <https://github.com/jmdana>, lasers
 @license GPLv3 <https://www.gnu.org/licenses/gpl-3.0.txt>
 
 SAMPLE OUTPUT
-{'color': '#00FF00', 'full_text': u'BT: Microsoft Bluetooth Notebook Mouse 5000'}
-
-off
-{'color': '#FF0000', 'full_text': u'BT'}
+{'color': '#00FF00', 'full_text': u'Microsoft Bluetooth Notebook Mouse 5000'}
 """
 
-from pydbus import SystemBus
-
-DEFAULT_FORMAT = "BT[: {format_device}]"
-
-
-def get_connected_devices():
-    bus = SystemBus()
-    manager = bus.get("org.bluez", "/")["org.freedesktop.DBus.ObjectManager"]
-    objects = manager.GetManagedObjects()
-    devices = []
-
-    for dev_path, interfaces in objects.items():
-        if "org.bluez.Device1" in interfaces.keys():
-            properties = objects[dev_path]["org.bluez.Device1"]
-
-            if properties["Connected"] == 1:
-                devices.append((properties["Address"], properties["Name"]))
-
-    return devices
+from gi.repository import Gio
 
 
 class Py3status:
@@ -59,87 +98,89 @@ class Py3status:
 
     # available configuration parameters
     cache_timeout = 10
-    format = DEFAULT_FORMAT
-    format_device = "{name}"
-    format_separator = "\|"
-
-    class Meta:
-        def deprecate_function(config):
-            if not config.get("format_separator") and config.get("device_separator"):
-                sep = config.get("device_separator")
-                sep = sep.replace("\\", "\\\\")
-                sep = sep.replace("[", "\[")
-                sep = sep.replace("]", "\]")
-                sep = sep.replace("|", "\|")
-
-                return {"format_separator": sep}
-            else:
-                return {}
-
-        deprecated = {
-            "function": [{"function": deprecate_function}],
-            "remove": [
-                {
-                    "param": "device_separator",
-                    "msg": "obsolete set using `format_separator`",
-                }
-            ],
-        }
+    format = "{format_adapter}"
+    format_adapter = "{format_device}"
+    format_adapter_separator = " "
+    format_device = "\?if=connected&color=connected {alias}"
+    format_device_separator = " "
+    thresholds = [(False, "bad"), (True, "good")]
 
     def post_config_hook(self):
-        # DEPRECATION WARNING. SPECIAL CASE. DO NOT USE THIS AS EXAMPLE.
-        format_prefix = getattr(self, "format_prefix", None)
-        format_no_conn = getattr(self, "format_no_conn", None)
-        format_no_conn_prefix = getattr(self, "format_no_conn_prefix", None)
+        bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
+        iface = "org.freedesktop.DBus.ObjectManager"
+        self.bluez_manager = Gio.DBusProxy.new_sync(
+            bus, Gio.DBusProxyFlags.NONE, None, "org.bluez", "/", iface, None
+        )
 
-        placeholders = set(self.py3.get_placeholders_list(self.format))
-        if set(["name", "mac"]) & placeholders:
-            # this is an old format so should be format_device
-            self.format_device = self.format
-            self.format = DEFAULT_FORMAT
-            msg = "DEPRECATION WARNING: your format is using invalid "
-            msg += "placeholders you should update your configuration."
-            self.py3.log(msg)
-
-        if self.format != DEFAULT_FORMAT:
-            # The user has set a format using the new style format so we are
-            # done here.
-            return
-
-        if format_prefix or format_no_conn_prefix or format_no_conn:
-            # create a format that will give the expected output
-            self.format = u"[\?if=format_device {}{{format_device}}|{}{}]".format(
-                format_prefix or "BT: ",
-                format_no_conn_prefix or "BT: ",
-                format_no_conn or "OFF",
+        self.thresholds_init = {}
+        for name in ["format", "format_adapter", "format_device"]:
+            self.thresholds_init[name] = self.py3.get_color_names_list(
+                getattr(self, name)
             )
-            msg = "DEPRECATION WARNING: you are using old style configuration "
-            msg += "parameters you should update to use the new format."
-            self.py3.log(msg)
+
+    def _get_bluez_data(self):
+        objects = self.bluez_manager.GetManagedObjects()
+        filters = [("adapters", "org.bluez.Adapter1"), ("devices", "org.bluez.Device1")]
+        temporary = {}
+
+        for path, interfaces in sorted(objects.items()):
+            interface_keys = interfaces.keys()
+            for name, match in filters:
+                if match in interface_keys:
+                    interface = {k.lower(): v for k, v in interfaces[match].items()}
+                    interface.update({"path": path, "uuids": "[]"})
+                    temporary.setdefault(name, []).append(interface)
+                    break
+
+        for device in temporary.pop("devices", []):
+            for index, adapter in enumerate(temporary["adapters"]):
+                if device["adapter"] == adapter["path"]:
+                    temporary["adapters"][index].setdefault("devices", []).append(
+                        device
+                    )
+                    break
+
+        return temporary
 
     def bluetooth(self):
-        devices = get_connected_devices()
+        bluez_data = self._get_bluez_data()
+        adapters = bluez_data.pop("adapters", [])
+        new_adapter = []
 
-        if devices:
-            data = []
-            for mac, name in devices:
-                data.append(
-                    self.py3.safe_format(self.format_device, {"name": name, "mac": mac})
-                )
+        for adapter in adapters:
+            devices = adapter.pop("devices", [])
+            new_device = []
 
-            format_separator = self.py3.safe_format(self.format_separator)
-            format_device = self.py3.composite_join(format_separator, data)
-            color = self.py3.COLOR_GOOD
-        else:
-            format_device = None
-            color = self.py3.COLOR_BAD
+            for device in devices:
+                for x in self.thresholds_init["format_device"]:
+                    if x in device:
+                        self.py3.threshold_get_color(device[x], x)
+
+                new_device.append(self.py3.safe_format(self.format_device, device))
+
+            format_device_separator = self.py3.safe_format(self.format_device_separator)
+            format_device = self.py3.composite_join(format_device_separator, new_device)
+
+            adapter.update({"format_device": format_device, "device": len(devices)})
+
+            for x in self.thresholds_init["format_adapter"]:
+                if x in adapter:
+                    self.py3.threshold_get_color(adapter[x], x)
+
+            new_adapter.append(self.py3.safe_format(self.format_adapter, adapter))
+
+        format_adapter_separator = self.py3.safe_format(self.format_adapter_separator)
+        format_adapter = self.py3.composite_join(format_adapter_separator, new_adapter)
+
+        bluetooth_data = {"format_adapter": format_adapter, "adapter": len(adapters)}
+
+        for x in self.thresholds_init["format"]:
+            if x in bluetooth_data:
+                self.py3.threshold_get_color(bluetooth_data[x], x)
 
         return {
             "cached_until": self.py3.time_in(self.cache_timeout),
-            "full_text": self.py3.safe_format(
-                self.format, {"format_device": format_device}
-            ),
-            "color": color,
+            "full_text": self.py3.safe_format(self.format, bluetooth_data),
         }
 
 
