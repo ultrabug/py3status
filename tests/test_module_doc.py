a2fd5238b9d84145a61464de447ec696d6aaa73a
Maxim Baz <git@maximbaz.com>
2020-01-07 20:51:39 +0100

    Fix formatting

--- tests/test_module_doc.py
+++ tests/test_module_doc.py
@@ -33,7 +33,12 @@ OBSOLETE_PARAM = []
 
 RE_PARAM = re.compile(r"^  - `(?P<name>[^`]*)`.*?(\*\(default (?P<value>(.*))\)\*)?$")
 
-AST_LITERAL_TYPES = {"Num": "n", "Str": "s", "NameConstant": "value", "Constant": "value"}
+AST_LITERAL_TYPES = {
+    "Num": "n",
+    "Str": "s",
+    "NameConstant": "value",
+    "Constant": "value",
+}
 
 MODULE_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "..", "py3status", "modules"
