a8ae250e0c84c53d4f1f681c210c5dcc807d8c81
Ultrabug <ultrabug@gentoo.org>
2021-06-29 16:31:48 +0200

    arch_updates: fix black formatting

--- py3status/modules/arch_updates.py
+++ py3status/modules/arch_updates.py
@@ -47,7 +47,9 @@ class Py3status:
     def post_config_hook(self):
         helper = {
             "pacman": self.py3.check_commands(["checkupdates"]),
-            "aur": self.py3.check_commands(["auracle", "trizen", "yay", "cower", "paru"]),
+            "aur": self.py3.check_commands(
+                ["auracle", "trizen", "yay", "cower", "paru"]
+            ),
         }
         if self.format:
             placeholders = self.py3.get_placeholders_list(self.format)
