480710773847ae0fb11065e05376abe6818c8928
Valentin Weber <mail@weberval.de>
2021-07-17 01:20:17 +0200

    fixed module sway_idle to pass tox

--- py3status/modules/sway_idle.py
+++ py3status/modules/sway_idle.py
@@ -6,6 +6,7 @@ This Module shows an indicator, if an idle is inhibited by an inhibitor.
 For more information about inhibit idle see `man 5 sway`
 
 Configuration parameters:
+    cache_timeout: How often we refresh this module in seconds (default 1)
     format: Display format (default 'Inhibit Idle: [\?if=inhibit_idle=True True]|False')
 
 Format placeholders:
@@ -21,23 +22,34 @@ sway_idle {
 
 @author Valentin Weber <valentin+py3status@wv2.ch>
 @license BSD
+
+SAMPLE OUTPUT
+{full_text': 'Inhibit Idle: True'}
 """
+
 
 class Py3status:
 
-    format = 'Inhibit Idle: [\?if=inhibit_idle=True True]|False'
+    # available configuration parameters
+    cache_timeout = 1
+    format = "Inhibit Idle: [\?if=inhibit_idle=True True]|False"
 
     def sway_idle(self):
-        sway_tree = self.py3.command_output(self.py3.get_wm_msg() + ' -t get_tree')
-        inhibit_idle = str(sway_tree.find('inhibit_idle": true') > 0)
+        sway_tree = self.py3.command_output(self.py3.get_wm_msg() + " -t get_tree")
+        inhibit_idle = str(sway_tree.find('"inhibit_idle": true') > 0)
         return {
-            'full_text': self.py3.safe_format(self.format,param_dict={'inhibit_idle': inhibit_idle}),
-            'cached_until': self.py3.time_in(seconds=1)
+            "full_text": self.py3.safe_format(
+                self.format, param_dict={"inhibit_idle": inhibit_idle}
+            ),
+            "cached_until": self.py3.time_in(self.cache_timeout),
         }
+
 
 if __name__ == "__main__":
     """
     Run module in test mode.
     """
+
     from py3status.module_test import module_test
+
     module_test(Py3status)
