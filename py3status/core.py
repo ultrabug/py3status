ae4ccbc4bbeed87f23f87f2b8acbdcc3d251fa01
Rafael Lerm <lerm@google.com>
2021-08-22 21:01:46 -0300

    Use the logging module directly instead of home-grown code.
    This has the advantage of overall less custom code; as well as support
    for per-module configuration. This would enable a potential solution for
    https://github.com/ultrabug/py3status/issues/1479, since in the future
    it can allow per-module configuration of log levels.
    
    I expect this to mainly help module creators, allowing them to enable
    logging for only their module, while disabling all other messages. It
    also is easier to use, since the `logging` module's interface is
    generally simpler than `self.py3`.
    
    Here, I've made the decision to keep the message format as close as
    possible to the existing log messages.

--- py3status/core.py
+++ py3status/core.py
@@ -29,6 +29,12 @@ LOGGING_LEVELS = {
     "info": logging.INFO,
 }
 
+LOGGING_LEVELS = {
+        "error": logging.ERROR,
+        "warning": logging.WARNING,
+        "info": logging.INFO,
+}
+
 DBUS_LEVELS = {"error": "critical", "warning": "normal", "info": "low"}
 
 CONFIG_SPECIAL_SECTIONS = [
