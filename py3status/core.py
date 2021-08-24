7d8f5b948e2d74ed563f77bc30e83fc8bbc4faa7
Rafael Lerm <lerm@google.com>
2021-08-24 18:49:03 -0300

    Use standard logging infrastructure on "debug" statements.
    Instead of checking for the "debug" option, we just log with the DEBUG
    level. The root logger's configuration will suppress log levels
    as needed.

--- py3status/core.py
+++ py3status/core.py
@@ -28,12 +28,6 @@ LOGGING_LEVELS = {
     "info": logging.INFO,
 }
 
-LOGGING_LEVELS = {
-        "error": logging.ERROR,
-        "warning": logging.WARNING,
-        "info": logging.INFO,
-}
-
 DBUS_LEVELS = {"error": "critical", "warning": "normal", "info": "low"}
 
 CONFIG_SPECIAL_SECTIONS = [
