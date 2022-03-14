3e48202f7c9147f7759fc945a6512007766bb234
Ultrabug <ultrabug@users.noreply.github.com>
2022-03-14 10:30:18 +0100

    announce support for python 3.10 (#2111)

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
